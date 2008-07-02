f65f7651de17f2a9bb591f49ce8ec22676f3dee1
keontre <keontre>
2008-07-02 00:09:39 +0000

    Formatted recipe_purge.sql and added grant for BDS_APP.

--- SOURCE/PDB/BESPOKE/MCA/ORACLE/MANU_APP/CODE/recipe_purge.sql
+++ SOURCE/PDB/BESPOKE/MCA/ORACLE/MANU_APP/CODE/recipe_purge.sql
@@ -1,8 +1,6 @@
-DROP PROCEDURE MANU_APP.RECIPE_PURGE;
-
-CREATE OR REPLACE PROCEDURE MANU_APP.Recipe_Purge IS
+create or replace procedure manu_app.recipe_purge is
 /******************************************************************************
-   NAME:       Delete_Recpe
+   NAME:       recipe_purge
    PURPOSE:    
    REVISIONS:
    Ver        Date        Author           Description
@@ -12,42 +10,37 @@ CREATE OR REPLACE PROCEDURE MANU_APP.Recipe_Purge IS
    This procedure will delete all records from the RECPE_.. tables
    where they don't exist in the main proc_order table
 ******************************************************************************/
+begin
+  /*-*/
+  /* delete recipe records 
+  /*-*/
+  delete 
+  from recpe_dtl
+  where cntl_rec_id not in (select cntl_rec_id from cntl_rec);
 
-   
-BEGIN
-	/*-*/
-	/* delete recipe records 
-	/*-*/
-   DELETE FROM recpe_dtl
-	 WHERE cntl_rec_id NOT IN (SELECT cntl_rec_id FROM cntl_rec);
+  delete 
+  from recpe_resrce
+  where cntl_rec_id not in (select cntl_rec_id from cntl_rec);
 
-   DELETE FROM recpe_resrce
-    WHERE cntl_rec_id NOT IN (SELECT cntl_rec_id FROM cntl_rec);
-
-   DELETE FROM recpe_val
-    WHERE cntl_rec_id NOT IN (SELECT cntl_rec_id FROM cntl_rec);
+  delete 
+  from recpe_val
+  where cntl_rec_id not in (select cntl_rec_id from cntl_rec);
+  	 
+  delete 
+  from recpe_hdr
+  where cntl_rec_id not in (select cntl_rec_id from cntl_rec);	 
 	 
-   DELETE FROM recpe_hdr
-    WHERE cntl_rec_id NOT IN (SELECT cntl_rec_id FROM cntl_rec);
-	 
-	 
-	COMMIT;
+  commit;
 	
-   EXCEPTION
-     WHEN OTHERS THEN
-       ROLLBACK;
-       RAISE_APPLICATION_ERROR(-20000, 'Recipe_Purge failed ' 
-			 || 'Oracle error ' || SUBSTR(SQLERRM, 1, 1000));
-END Recipe_Purge;
-/
+exception
+  when others then
+    rollback;
+    raise_application_error(-20000, 'Recipe_Purge failed ' || 'Oracle error ' || substr(sqlerrm, 1, 1000));
+       
+end recipe_purge;
 
+create or replace public synonym recipe_purge for manu_app.recipe_purge;
 
-DROP PUBLIC SYNONYM RECIPE_PURGE;
-
-CREATE PUBLIC SYNONYM RECIPE_PURGE FOR MANU_APP.RECIPE_PURGE;
-
-
-GRANT EXECUTE ON MANU_APP.RECIPE_PURGE TO APPSUPPORT;
-
-GRANT EXECUTE ON MANU_APP.RECIPE_PURGE TO LICS_APP;
-
+grant execute on manu_app.recipe_purge to appsupport;
+grant execute on manu_app.recipe_purge to bds_app;
+grant execute on manu_app.recipe_purge to lics_app;
