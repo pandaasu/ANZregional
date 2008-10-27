40778b1b4fb031297a4eae6e59bdeab422b136a1
keontre <keontre>
2008-10-27 00:18:00 +0000

    Added to production as per WO #147601

---
+++ SOURCE/PDB/BESPOKE/MFA/ORACLE/PT_APP/common.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE PACKAGE PT_APP.Common AS
+/******************************************************************************
+   NAME:       Common
+   PURPOSE:
+
+   REVISIONS:
+   Ver        Date            Author              Description
+   ---------  -------------  ------------------  ------------------------------------
+   1.0        22-Oct-2007    Scott R. Harding     Created this package.
+******************************************************************************/
+
+   -- Default Reference Return Cursor.
+  TYPE RETURN_REF_CURSOR IS REF CURSOR;
+
+END Common;
+/
+
+
