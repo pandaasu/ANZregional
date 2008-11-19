b494b4fff7b1733de9eb9e19e714889884ab4a41
keontre <keontre>
2008-11-19 00:51:05 +0000

    Updated ics_std_code.inc files to handle Single Sign On (SSO) which included MARSDOM before the 5/3. Added SHLIB to the ics_loader.config files. Added ATLCIS09 for AU41/42-FG in mqs_trigger_WODU03T1.config. Added CUC folder for webpage files.

---
+++ SOURCE/LICS/BASE/ORACLE/INSTALL/MFANZ/lics_sec_interface_init.sql
@@ -0,0 +1,63 @@
+delete lics_sec_interface;
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','PRCICS01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','PRCICS01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','PRCICS01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','PRCICS01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','PRCICS01');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','PRCICS02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','PRCICS02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','PRCICS02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','PRCICS02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','PRCICS02');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','PRCICS03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','PRCICS03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','PRCICS03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','PRCICS03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','PRCICS03');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','PRCICS04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','PRCICS04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','PRCICS04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','PRCICS04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','PRCICS04');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','PRCICS05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','PRCICS05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','PRCICS05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','PRCICS05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','PRCICS05');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','ICSKRN01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','ICSKRN01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','ICSKRN01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','ICSKRN01');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','ICSKRN01');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','ICSKRN02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','ICSKRN02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','ICSKRN02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','ICSKRN02');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','ICSKRN02');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','ICSKRN03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','ICSKRN03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','ICSKRN03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','ICSKRN03');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','ICSKRN03');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','ICSKRN04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','ICSKRN04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','ICSKRN04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','ICSKRN04');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','ICSKRN04');
+
+insert into lics_sec_interface(sei_user, sei_interface) values ('GLENLIN','ICSKRN05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('KEONTRE','ICSKRN05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GIRLIJON','ICSKRN05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('GREGASTE','ICSKRN05');
+insert into lics_sec_interface(sei_user, sei_interface) values ('HARDISCO','ICSKRN05');
+
+commit;
