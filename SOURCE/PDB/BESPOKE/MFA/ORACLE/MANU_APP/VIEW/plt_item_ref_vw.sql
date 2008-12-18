5ff8d1b5ddaa99fce1e7430ab0c5d1423d7089f5
keontre <keontre>
2008-12-18 23:27:30 +0000

    Updated view plt_item_ref_vw.sql as per WO #155832.

--- SOURCE/PDB/BESPOKE/MFA/ORACLE/MANU_APP/VIEW/plt_item_ref_vw.sql
+++ SOURCE/PDB/BESPOKE/MFA/ORACLE/MANU_APP/VIEW/plt_item_ref_vw.sql
@@ -1,56 +1,56 @@
-DROP VIEW MANU_APP.PLT_ITEM_REF_VW;
+create or replace view plt_item_ref_vw as 
+  select ltrim(m.material_code,'0') as material_code, 
+    m.material_desc as material_desc, 
+    m.material_type, 
+    m.old_material_code as old_item_code, 
+    m.uom, 
+    m.gross_wght, 
+    m.dclrd_wght, 
+    m.dclrd_uom, 
+    m.ean_code, 
+    m.shelf_life, 
+    m.eff_start_date, 
+    p.units_per_case_date, 
+    p.apn_code, 
+    p.units_per_case, 
+    p.inners_per_case_date, 
+    p.inners_per_case, 
+    p.pi_start_date, 
+    p.pi_end_date, 
+    p.pllt_gross_wght, 
+    p.crtns_per_pllt, 
+    p.crtns_per_layer, 
+    p.uom_qty, 
+    x.carton_label_format, 
+    '' as trade_partner_prod_code, 
+    m.batch_mngmnt_rqrmnt_indctr, 
+    m.store_locn,
+    s.mcu_material_code,
+    s.mcu_description,
+    s.mcu_ean,
+    s.mcu_gross_weight
+  from material m, 
+    material_pllt p, 
+    site_matl_cluster_xref x, 
+    (
+      select b.material as material_code, 
+        b.sub_matl as mcu_material_code, 
+        m.bds_material_desc_en as mcu_description, 
+        m.interntl_article_no as mcu_ean, 
+        m.gross_weight as mcu_gross_weight 
+      from bom b, 
+        bds_material_plant_mfanz m 
+      where b.sub_matl = ltrim(m.sap_material_code,'0') 
+        and b.plant = m.plant_code 
+        and m.mars_merchandising_unit_flag = 'X'
+    ) s 
+  where m.material_code = p.matl_code(+) 
+    and m.material_code = x.material_code(+) 
+    and m.material_code = s.material_code(+);
+/
 
-/* Formatted on 2008/09/05 10:49 (Formatter Plus v4.8.8) */
-CREATE OR REPLACE FORCE VIEW manu_app.plt_item_ref_vw (material_code,
-                                                       material_desc,
-                                                       material_type,
-                                                       old_item_code,
-                                                       uom,
-                                                       gross_wght,
-                                                       dclrd_wght,
-                                                       dclrd_uom,
-                                                       ean_code,
-                                                       shelf_life,
-                                                       eff_start_date,
-                                                       units_per_case_date,
-                                                       apn_code,
-                                                       units_per_case,
-                                                       inners_per_case_date,
-                                                       inners_per_case,
-                                                       pi_start_date,
-                                                       pi_end_date,
-                                                       pllt_gross_wght,
-                                                       crtns_per_pllt,
-                                                       crtns_per_layer,
-                                                       uom_qty,
-                                                       carton_label_format,
-                                                       trade_partner_prod_code,
-                                                       batch_mngmnt_rqrmnt_indctr,
-                                                       store_locn
-                                                      )
-AS
-  SELECT LTRIM (m.material_code, '0') material_code,
-         m.material_desc material_desc, m.material_type,
-         m.old_material_code old_item_code, m.uom, m.gross_wght, m.dclrd_wght,
-         m.dclrd_uom, m.ean_code, m.shelf_life, m.eff_start_date,
-         p.units_per_case_date, p.apn_code, p.units_per_case,
-         p.inners_per_case_date, p.inners_per_case, p.pi_start_date,
-         p.pi_end_date, p.pllt_gross_wght, p.crtns_per_pllt,
-         p.crtns_per_layer, p.uom_qty, carton_label_format,
-         '' trade_partner_prod_code, m.batch_mngmnt_rqrmnt_indctr,
-         m.store_locn
-    FROM material m, material_pllt p, site_matl_cluster_xref x
-   WHERE m.material_code = p.matl_code(+) AND m.material_code = x.material_code(+);
+grant select on manu_app.plt_item_ref_vw to manu_user;
+grant select on manu_app.plt_item_ref_vw to negusian with grant option;
+grant select on manu_app.plt_item_ref_vw to pt_app with grant option;
 
-
-DROP PUBLIC SYNONYM PLT_ITEM_REF_VW;
-
-CREATE PUBLIC SYNONYM PLT_ITEM_REF_VW FOR MANU_APP.PLT_ITEM_REF_VW;
-
-
-GRANT SELECT ON MANU_APP.PLT_ITEM_REF_VW TO MANU_USER;
-
-GRANT SELECT ON MANU_APP.PLT_ITEM_REF_VW TO NEGUSIAN WITH GRANT OPTION;
-
-GRANT SELECT ON MANU_APP.PLT_ITEM_REF_VW TO PT_APP WITH GRANT OPTION;
-
+create or replace public synonym plt_item_ref_vw for manu_app.plt_item_ref_vw;
