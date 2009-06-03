37b0dbcdf7286e840a0c9938b1aab3cbead8652c
keontre <keontre>
2009-06-03 01:18:30 +0000

    Implemented RVR filters as per CR #48393

--- SOURCE/PDB/BESPOKE/WGI/ORACLE/MANU_APP/VIEW/recipe_fcs_vw.sql
+++ SOURCE/PDB/BESPOKE/WGI/ORACLE/MANU_APP/VIEW/recipe_fcs_vw.sql
@@ -1,74 +1,92 @@
-DROP VIEW MANU_APP.RECIPE_FCS_VW;
+/******************************************************************************/
+/* View Definition                                                           */
+/******************************************************************************/
+/**
+ System  : manu 
+ View   : recipe_fcs_vw
+ Owner   : manu_app
+ Author  : Unknown 
 
-/* Formatted on 2008/11/05 13:18 (Formatter Plus v4.8.8) */
-CREATE OR REPLACE FORCE VIEW manu_app.recipe_fcs_vw (proc_order,
-                                                     cntl_rec_id,
-                                                     material,
-                                                     material_text,
-                                                     resource_code,
-                                                     operation,
-                                                     phase,
-                                                     seq,
-                                                     code,
-                                                     description,
-                                                     VALUE,
-                                                     uom,
-                                                     mpi_type,
-                                                     run_start_datime,
-                                                     run_end_datime,
-                                                     proc_order_status,
-                                                     batch_qty,
-                                                     storage_locn
-                                                    )
-AS
-  SELECT LTRIM (v.proc_order, 0) proc_order, cntl_rec_id,
-         LTRIM (material, '0') material, c.material_text, r.resource_code,
-         v.operation, TO_NUMBER (phase) phase,
-         TO_NUMBER (LPAD (seq, 4, 0)) seq, LTRIM (material_code, '0') code,
-         material_desc description,
-         
-         --TO_CHAR(ROUND(TO_NUMBER(MATERIAL_QTY),3),'9999999.990') VALUE,
-         TO_CHAR (get_bom_qty (LTRIM (c.material, '0'),
-                               LTRIM (v.material_code, '0'),
-                               v.seq
-                              ),
-                  '9999990.990'
-                 ) VALUE,
-         material_uom uom, 'M' mpi_type, run_start_datime, run_end_datime,
-         s.closed proc_order_status,
-         TO_CHAR (get_bom_batch_qty (LTRIM (c.material, '0'))) batch_qty,
-         storage_locn
-    FROM cntl_rec_bom v, cntl_rec_resource r, cntl_rec c,
-         cntl_rec_status_vw s
-   WHERE r.proc_order = v.proc_order
-     AND r.operation = v.operation
-     AND c.proc_order = v.proc_order
-     AND TO_NUMBER (r.proc_order) = TO_NUMBER (s.proc_order(+))
-     AND teco_status = 'NO'
-  UNION ALL
-  SELECT LTRIM (r.proc_order, 0) proc_order, c.cntl_rec_id,
-         LTRIM (c.material, '0') material, c.material_text, r.resource_code,
-         v.operation, TO_NUMBER (phase) phase,
-         TO_NUMBER (LPAD (seq, 4, 0)) seq, mpi_tag code, mpi_desc description,
-         mpi_val VALUE, mpi_uom uom, 'V', run_start_datime, run_end_datime,
-         s.closed proc_order_status,
-         TO_CHAR (get_bom_batch_qty (LTRIM (c.material, '0'))) batch_qty,
-         storage_locn
-    FROM cntl_rec_mpi_val v,
-         cntl_rec_resource r,
-         cntl_rec c,
-         cntl_rec_status_vw s
-   WHERE r.proc_order = v.proc_order
-     AND r.operation = v.operation
-     AND r.proc_order = c.proc_order
-     AND TO_NUMBER (r.proc_order) = TO_NUMBER (s.proc_order(+))
-     AND teco_status = 'NO';
+ Description 
+ ----------- 
+ Manufacturing - Recipe Factory Control System View
 
+ YYYY/MM   Author         Description 
+ -------   ------         ----------- 
+ ????/??   Unknown        Created
+ 2009/05   Trevor Keon    Added filter for RVR process orders 
 
-DROP PUBLIC SYNONYM RECIPE_FCS_VW;
+*******************************************************************************/
 
-CREATE PUBLIC SYNONYM RECIPE_FCS_VW FOR MANU_APP.RECIPE_FCS_VW;
+/**/
+/* View creation 
+/**/
+create or replace force view manu_app.recipe_fcs_vw as
+  select ltrim (v.proc_order, 0) as proc_order, 
+    cntl_rec_id,
+    ltrim(material, '0') as material, 
+    c.material_text, 
+    r.resource_code,
+    v.operation, 
+    to_number(phase) as phase,
+    to_number(lpad(seq, 4, 0)) as seq, 
+    ltrim (material_code, '0') as code,
+    material_desc as description,
+    to_char(get_bom_qty(ltrim (c.material, '0'), ltrim (v.material_code, '0'), v.seq), '9999990.990') as value,
+    material_uom uom, 
+    'M' as mpi_type, 
+    run_start_datime, 
+    run_end_datime,
+    s.closed as proc_order_status,
+    to_char (get_bom_batch_qty (ltrim (c.material, '0'))) as batch_qty,
+    storage_locn
+  from cntl_rec_bom v, 
+    cntl_rec_resource r, 
+    cntl_rec c,
+    cntl_rec_status_vw s
+  where r.proc_order = v.proc_order
+    and r.operation = v.operation
+    and c.proc_order = v.proc_order
+    and to_number (r.proc_order) = to_number (s.proc_order(+))
+    and teco_status = 'NO'
+    and substr(c.proc_order, 1, 1) between '0' and '9'
+  union all
+  select ltrim (r.proc_order, 0) as proc_order, 
+    c.cntl_rec_id,
+    ltrim (c.material, '0') as material, 
+    c.material_text, 
+    r.resource_code,
+    v.operation, 
+    to_number(phase) as phase,
+    to_number(lpad(seq, 4, 0)) as seq, 
+    mpi_tag as code, 
+    mpi_desc as description,
+    mpi_val as value, 
+    mpi_uom as uom, 
+    'V' as mpi_type, 
+    run_start_datime, 
+    run_end_datime,
+    s.closed as proc_order_status,
+    to_char (get_bom_batch_qty (ltrim (c.material, '0'))) as batch_qty,
+    storage_locn
+  from cntl_rec_mpi_val v,
+    cntl_rec_resource r,
+    cntl_rec c,
+    cntl_rec_status_vw s
+  where r.proc_order = v.proc_order
+    and r.operation = v.operation
+    and r.proc_order = c.proc_order
+    and to_number (r.proc_order) = to_number (s.proc_order(+))
+    and teco_status = 'NO'
+    and substr(c.proc_order, 1, 1) between '0' and '9';
 
+/**/
+/* Authority 
+/**/
+grant select on manu_app.recipe_fcs_vw to bds_app with grant option;
+grant select on manu_app.recipe_fcs_vw to manu_user;
 
-GRANT SELECT ON MANU_APP.RECIPE_FCS_VW TO MANU_USER;
-
+/**/
+/* Synonym 
+/**/
+create or replace public synonym recipe_fcs_vw for manu_app.recipe_fcs_vw;
