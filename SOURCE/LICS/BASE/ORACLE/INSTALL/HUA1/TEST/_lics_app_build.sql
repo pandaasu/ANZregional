ab40a9fb0683ec3aedb9509e144f2cea62fc63d0
huangviv <huangviv>
2011-09-28 09:57:57 +0000

    Create HUA1 directory

---
+++ SOURCE/LICS/BASE/ORACLE/INSTALL/HUA1/TEST/_lics_app_build.sql
@@ -0,0 +1,130 @@
+/******************************************************************/
+/* System  : LICS                                                 */
+/* Object  : _lics_app_build                                      */
+/* Author  : Steve Gregan                                         */
+/* Date    : August 2011                                          */
+/*                                                                */
+/******************************************************************/
+/*  A. Scan for <SOURCE_PATH> and replace with your local path to the source repository up to but not including \SOURCE (eg. D:\Vivian\LADS\SourceRepository)
+/*  B. Scan for <DATABASE> and replace with the database name (eg. DB1296T.AP.MARS)
+/*  C. Scan for <LICS_APP_PASSWORD> and replace with the LICS_APP password
+/*  D. Scan for <INSTALLATION> and replace with the installation folder in the source repository (eg. NORTH_ASIA from the path <SOURCE_PATH>\SOURCE\LICS\BASE\ORACLE\INSTALL\NORTH_ASIA\TEST)
+/*  E. Scan for <ENVIRONMENT> and replace with the environment folder in the source repository (eg. TEST from the path <SOURCE_PATH>\SOURCE\LICS\BASE\ORACLE\INSTALL\NORTH_ASIA\TEST)
+/******************************************************************/
+
+/*-*/
+/* Set the echo off
+/*-*/
+set echo off;
+
+/*-*/
+/* Set the define character
+/*-*/
+set define ^;
+
+/*-*/
+/* Define the work variables
+/*-*/
+define pro_path = D:\Vivian\LADS\SourceRepository\SOURCE\LICS\BASE\ORACLE\LICS_APP\CODE
+define spl_path = D:\Vivian\LADS\SourceRepository\SOURCE\LICS\BASE\ORACLE\INSTALL\HUA1\TEST
+define database = DB1324T.AP.MARS
+define codeuser = LICS_APP
+define code_password = licsapp_dev
+
+/*-*/
+/* Start the spool process
+/*-*/
+spool ^spl_path\_lics_app_build.log
+
+/*-*/
+/* Compile the stored procedures
+/*-*/
+prompt CONNECTING (LICS_APP) ...
+
+connect ^codeuser/^code_password@^database
+
+prompt CREATING LICS_APP PROCEDURES ...
+
+/*-*/
+/* Ensure correct lics_parameter.sql file is used
+/*-*/
+@D:\Vivian\LADS\SourceRepository\SOURCE\LICS\BASE\ORACLE\INSTALL\HUA1\TEST\lics_parameter.sql;
+@^pro_path\lics_datastore_configuration_type.sql;
+@^pro_path\lics_datastore_type.sql;
+@^pro_path\lics_security_type.sql;
+@^pro_path\lics_spreadsheet_type.sql;
+@^pro_path\lics_stream_type.sql;
+@^pro_path\lics_constant.sql;
+@^pro_path\lics_documentation.sql;
+@^pro_path\lics_buffer.sql;
+@^pro_path\lics_file_name.sql;
+@^pro_path\lics_file_search.sql;
+@^pro_path\lics_form.sql;
+@^pro_path\lics_job_configuration.sql;
+@^pro_path\lics_group_configuration.sql;
+@^pro_path\lics_interface_configuration.sql;
+@^pro_path\lics_datastore_configuration.sql;
+@^pro_path\lics_routing_configuration.sql;
+@^pro_path\lics_security_configuration.sql;
+@^pro_path\lics_setting_configuration.sql;
+@^pro_path\lics_stream_configuration.sql;
+@^pro_path\lics_notification.sql;
+@^pro_path\lics_pipe.sql;
+@^pro_path\lics_locking.sql;
+@^pro_path\lics_logging.sql;
+@^pro_path\lics_mailer.sql;
+@^pro_path\lics_last_run_control.sql;
+@^pro_path\lics_datastore.sql;
+@^pro_path\lics_spreadsheet.sql;
+@^pro_path\lics_schedule_next.sql;
+@^pro_path\lics_schedule_tz.sql;
+@^pro_path\lics_time.sql;
+@^pro_path\lics_purging.sql;
+@^pro_path\lics_security.sql;
+@^pro_path\lics_remote_loader.sql;
+@^pro_path\lics_interface_search.sql;
+@^pro_path\lics_inbound_loader.sql;
+@^pro_path\lics_passthru_loader.sql;
+@^pro_path\lics_outbound_loader.sql;
+@^pro_path\lics_stream_loader.sql;
+@^pro_path\lics_trigger_loader.sql;
+@^pro_path\lics_trigger_submitter.sql;
+@^pro_path\lics_file_processor.sql;
+@^pro_path\lics_inbound_processor.sql;
+@^pro_path\lics_passthru_processor.sql;
+@^pro_path\lics_outbound_processor.sql;
+@^pro_path\lics_daemon_processor.sql;
+@^pro_path\lics_poller_processor.sql;
+@^pro_path\lics_stream_processor.sql;
+@^pro_path\lics_trigger_processor.sql;
+@^pro_path\lics_sap_processor.sql;
+@^pro_path\lics_job_processor.sql;
+@^pro_path\lics_file_poller.sql;
+@^pro_path\lics_stream_poller.sql;
+@^pro_path\lics_inbound_utility.sql;
+@^pro_path\lics_processing.sql;
+@^pro_path\lics_job_control.sql;
+@^pro_path\lics_interface_loader.sql;
+@^pro_path\lics_interface_process.sql;
+@^pro_path\lics_interface_view.sql;
+@^pro_path\lics_file_monitor.sql;
+@^pro_path\lics_router.sql;
+
+/*-*/
+/* Undefine the work variables
+/*-*/
+undefine pro_path
+undefine spl_path
+undefine database
+undefine codeuser
+undefine code_password
+
+/*-*/
+/* Stop the spool process
+/*-*/
+spool off;
+
+/*-*/
+/* Set the define character
+/*-*/
+set define &;
---
+++ SOURCE/LICS/BASE/ORACLE/INSTALL/HUA1/TEST/lics_parameter.sql
@@ -0,0 +1,103 @@
+/******************/
+/* Package Header */
+/******************/
+create or replace package lics_parameter as
+
+   /******************************************************************************/
+   /* Package Definition                                                         */
+   /******************************************************************************/
+   /**
+    System  : lics
+    Package : lics_parameter
+    Owner   : lics_app
+    Author  : Steve Gregan - January 2004
+
+    DESCRIPTION
+    -----------
+    Local Interface Control System - Parameters
+
+    YYYY/MM   Author         Description
+    -------   ------         -----------
+    2004/01   Steve Gregan   Created
+    2011/02   Steve Gregan   End point architecture version
+    2011/09   Vivian Huang   Change parameter for CAD
+
+   *******************************************************************************/
+
+/*-*/
+/*  A. Scan for <SYSTEM> and replace with the installation system code (eg. ICS, CARE, VENUS, PROMAX, etc))
+/*  B. Scan for <ICS_FILE_SYSTEM> and replace with the ICS file system path for the installation (eg. /ics/lad/test)
+/*  C. Scan for <DATABASE> and replace with the database name (eg. DB1296T.AP.MARS)
+/*  D. Scan for <INSTALLATION> and replace with the installation name (eg. NORTH_ASIA)
+/*  E. Scan for <ENVIRONMENT> and replace with the environment name (eg. TEST)
+/*  F. Scan for <WEBSITE> and replace with the website URL (eg. http://test-isc.clio.ap.mars/)
+/*  G. Scan for <AMI_PATH> and replace with the AMI software path on the file system (eg. /opt/apps/mqft_light/test/shell/)
+/*  H. Scan for <ICS_EMAIL> and replace with the ICS notification email address (eg. "ISI ICS Test Group"@smtp.ap.mars)
+/*-*/
+
+   /*-*/
+   /* Public parameters
+   /*-*/
+   system_code constant varchar2(10) := 'CAD';
+   system_unit constant varchar2(10) := 'HUA';
+   system_environment constant varchar2(20) := 'TEST';
+   system_url constant varchar2(128) := 'test.CAD.hua.ap.mars';
+   log_environment constant varchar2(128) := 'TEST';
+   log_database constant varchar2(128) := 'DB1324T.AP.MARS';
+   ics_path constant varchar2(128) := '/ics/CAD/test/';
+   ami_path constant varchar2(128) := '/opt/apps/mqft_light/test/shell/';
+   folder_delimiter constant varchar2(1) := '/';
+   file_attribute_command constant varchar2(64) := '/bin/chmod 777 <FILE>';
+
+   /*-*/
+   /* Public directory parameters
+   /*-*/
+   ics_inbound constant varchar2(128) := 'ICS_INBOUND';
+   ics_outbound constant varchar2(128) := 'ICS_OUTBOUND';
+   inbound_directory constant varchar2(128) := ics_path||'inbound'||folder_delimiter;
+   outbound_directory constant varchar2(128) := ics_path||'outbound'||folder_delimiter;
+   archive_directory constant varchar2(128) := ics_path||'archive'||folder_delimiter;
+   view_directory constant varchar2(128) := ics_path||'webview'||folder_delimiter;
+   script_directory constant varchar2(128) := ics_path||'bin'||folder_delimiter;
+   log_directory constant varchar2(128) := ics_path||'log'||folder_delimiter;
+   ami_logfile constant varchar2(128) := log_directory||'ics_integration.log';
+
+   /*-*/
+   /* Public fatal parameters
+   /*-*/
+   fatal_opr_alert constant varchar2(256) := null;
+   fatal_ema_group constant varchar2(64) := '"lee.plus"@smtp.ap.mars';
+
+   /*-*/
+   /* Public email parameters
+   /*-*/
+   email_smtp_host constant varchar2(64) := 'smtp.ap.mars';
+   email_smtp_port constant number(2,0) := 25;
+
+   /*-*/
+   /* Public inbound parameters
+   /*-*/
+   inbound_line_max constant number(5,0) := 4000;
+   inbound_array_size constant number(5,0) := 200;
+
+   /*-*/
+   /* Public purge parameters
+   /*-*/
+   purge_event_history_days constant number(5,0) := 14;
+   purge_log_history_days constant number(5,0) := 14;
+
+   /*-*/
+   /* Public script parameters
+   /*-*/
+   purge_file_script constant varchar2(128) := script_directory||'ics_cleanup.sh';
+   restore_script constant varchar2(128) := script_directory||'ics_restore.sh';
+   inbound_sap_script constant varchar2(128) := script_directory||'ics_inbound_sap.sh';
+
+end lics_parameter;
+/  
+
+/**************************/
+/* Package Synonym/Grants */
+/**************************/
+create or replace public synonym lics_parameter for lics_app.lics_parameter;
+grant execute on lics_parameter to public;
