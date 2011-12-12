abb9ea462af25f8d5188f71d57c54f167e80a5ad
halicben <halicben>
2011-12-12 00:49:22 +0000

    Corrected java class path

--- SOURCE/LICS/BASE/ORACLE/LICS_APP/CODE/java_utility.sql
+++ SOURCE/LICS/BASE/ORACLE/LICS_APP/CODE/java_utility.sql
@@ -27,6 +27,7 @@
  YYYY/MM   Author         Description
  -------   ------         -----------
  2004/01   Steve Gregan   Created
+ 2011/12   Ben Halicki	  Updated java class path for V2
 
 *******************************************************************************/
 
@@ -53,13 +54,13 @@ create or replace package body java_utility as
    /* This procedure performs the external procedure execution */
    /************************************************************/
    procedure execute_external_procedure(par_command in varchar2)
-      as language java name 'com.isi.utility.ExternalProcess.executeProcedure(java.lang.String)';
+      as language java name 'com.isi.ics.ExternalProcess.executeProcedure(java.lang.String)';
 
    /**********************************************************/
    /* This function performs the external function execution */
    /**********************************************************/
    function execute_external_function(par_command in varchar2) return varchar2
-      as language java name 'com.isi.utility.ExternalProcess.executeFunction(java.lang.String) return java.lang.String';
+      as language java name 'com.isi.ics.ExternalProcess.executeFunction(java.lang.String) return java.lang.String';
 
 end java_utility;
 /
