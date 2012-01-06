1737254b5805f976cbf7740023136d43cd0f5455
halicben <halicben>
2012-01-06 02:13:12 +0000

    Added manu_constants.sql

---
+++ SOURCE/PDB/BASE/NODE/ORACLE/MANU_APP/CODE/manu_constants.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE PACKAGE MANU_APP.MANU_CONSTANTS AS
+
+   /**/
+   /* Public recipe constants
+   /**/
+   recipe_phantom      CONSTANT varchar2(18) := '1048800';
+
+END MANU_CONSTANTS;
+/
