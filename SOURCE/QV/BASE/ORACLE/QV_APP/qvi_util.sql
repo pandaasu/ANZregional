4dbaa091301894ae6594ad2ea2f4a60d04896328
chambmal <chambmal>
2012-03-29 11:44:26 +0000

    Update qvi util

--- SOURCE/QV/BASE/ORACLE/QV_APP/qvi_util.sql
+++ SOURCE/QV/BASE/ORACLE/QV_APP/qvi_util.sql
@@ -25,6 +25,7 @@ create or replace package qv_app.qvi_util as
    /*-*/
    function get_validated_string(par_column_name in varchar2, par_validation_descr in varchar2, par_validation_regexp in varchar2, par_validation_regexp_switch in varchar2, par_trim_flag in boolean, par_src_error in out boolean) return varchar2;
    function get_validated_string(par_column_name in varchar2, par_column_value in varchar2, par_validation_descr in varchar2, par_validation_regexp in varchar2, par_validation_regexp_switch in varchar2, par_trim_flag in boolean, par_src_error in out boolean) return varchar2;
+   function get_csv_at_position(par_csv_string in varchar2, par_csv_position number) return varchar2;
    
 end qvi_util;
 /
@@ -154,6 +155,60 @@ create or replace package body qv_app.qvi_util as
    /*-------------*/
    end get_validated_string;
 
+   function get_csv_at_position(par_csv_string in varchar2, par_csv_position number) return varchar2 as
+      /*-*/
+      /* Local definitions
+      /*-*/
+      var_return varchar2(4000);
+
+   /*-------------*/
+   /* Begin block */
+   /*-------------*/
+   begin
+      /*-*/
+      /* Get CSV position .. parsing CSV string with embedded commas, double quotes and line breaks
+      /*
+      /* *** Regular Expression taken from Kim Anthony Gentes 
+      /*     "Regex Pattern for Parsing CSV files with Embedded commas, double quotes and line breaks" .. 
+      /*-*/
+      var_return := regexp_substr(par_csv_string, '("(?:[^"]|"")*"|[^,]*),("(?:[^"]|"")*"|[^,]*)', 1, par_csv_position);
+      
+      /*-*/
+      /* Trim leading [comma quote] and [comma] combinations
+      /*-*/
+      if substr(var_return,1,2) = ',"' then
+         var_return := substr(var_return,3);
+      elsif substr(var_return,1,1) = ',' then
+         var_return := substr(var_return,2);
+      end if;
+
+      /*-*/
+      /* Trim trailing [quote]
+      /*-*/
+      if substr(var_return,length(var_return),1) = '"' then
+         var_return := substr(var_return,1,length(var_return)-1);
+      end if;
+      
+      return var_return;
+   exception
+
+      /*-*/
+      /* Exception trap
+      /*-*/
+      when others then
+
+         /*-*/
+         /* Raise an exception to the calling application
+         /*-*/
+         raise_application_error(-20000, 'FATAL ERROR - [QV_UTIL.GET_CSV_AT_POSITION] - '
+            ||'"CSV String "'||par_csv_string
+            ||'", CSV Position "'||par_csv_position
+            ||'" - '||substr(sqlerrm, 1, 1536));
+   /*-------------*/
+   /* End routine */
+   /*-------------*/
+   end get_csv_at_position;
+   
 end qvi_util;
 /
 
