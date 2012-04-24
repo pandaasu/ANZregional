056c0efa443d6cbf891964daaeb75ddaad88b721
chambmal <chambmal>
2012-04-24 02:03:57 +0000

    Moved QVI to its own tree ..

--- SOURCE/QV/BASE/ORACLE/QV_APP/qvi_xml_type.sql
+++
@@ -1,8 +0,0 @@
-/*****************/
-/* Package Types */
-/*****************/
-create or replace type qvi_xml_object as object (xml_text varchar2(2000 char));
-/
-
-create or replace type qvi_xml_type as table of qvi_xml_object;
-/
