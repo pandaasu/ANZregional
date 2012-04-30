af8f03c9fa9e4aad75a9eef952b5f5fa911aee58
chambmal <chambmal>
2012-04-30 05:31:02 +0000

    /* Modified: April 2012, Mal Chambeyron                           */ /*           Added fact time part (qvi_fac_tpar.sql)              */ /*           and update sequence (qvi_update_sequence.sql)        */

--- SOURCE/QVI/BASE/ORACLE/_qvi_qv_build.sql
+++ SOURCE/QVI/BASE/ORACLE/_qvi_qv_build.sql
@@ -4,6 +4,10 @@
 /* Author  : Steve Gregan                                         */
 /* Date    : March 2012                                           */
 /*                                                                */
+/* Modified: April 2012, Mal Chambeyron                           */
+/*           Added fact time part (qvi_fac_tpar.sql)              */
+/*           and update sequence (qvi_update_sequence.sql)        */
+/*                                                                */
 /******************************************************************/
 
 /*------------------------------*/
@@ -23,8 +27,8 @@ set define ^;
 /*-*/
 /* Define the work variables
 /*-*/
-define tab_path = C:\ISI_SVN\regional\SOURCE\QV\BASE\ORACLE\QV
-define spl_path = C:\ISI_SVN\regional\SOURCE\QV\BASE\ORACLE
+define tab_path = C:\ISI_SVN\regional\SOURCE\QVI\BASE\ORACLE\QV
+define spl_path = C:\ISI_SVN\regional\SOURCE\QVI\BASE\ORACLE
 
 /*-*/
 /* Start the spool process
@@ -40,12 +44,20 @@ prompt CREATING QVI QV TABLES ...
 @^tab_path\qvi_fac_defn.sql;
 @^tab_path\qvi_fac_time.sql;
 @^tab_path\qvi_fac_part.sql;
+@^tab_path\qvi_fac_tpar.sql;
 @^tab_path\qvi_fac_hedr.sql;
 @^tab_path\qvi_fac_data.sql;
 @^tab_path\qvi_src_hedr.sql;
 @^tab_path\qvi_src_data.sql;
 @^tab_path\qvi_dim_defn.sql;
 @^tab_path\qvi_dim_data.sql;
+
+/*-*/
+/* Compile the tables
+/*-*/
+prompt CREATING QVI UPDATE SEQUENCE ...
+
+@^tab_path\qvi_update_sequence.sql;
 
 /*-*/
 /* Undefine the work variables
