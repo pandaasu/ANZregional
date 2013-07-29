b643e6a0b57dd6876e6a2d2cc33a78570bbe9828
hornchr <hornchr>
2013-07-29 00:35:16 +0000

    Checked in the first working version of TV Activity.

--- SOURCE/LOGR/BASE/ORACLE/ODS_APP/LOGRWOD06_LOADER.body.sql
+++ SOURCE/LOGR/BASE/ORACLE/ODS_APP/LOGRWOD06_LOADER.body.sql
@@ -4,23 +4,13 @@ PACKAGE body LOGRWOD06_LOADER AS
 /*******************************************************************************
   Interface Field Definitions
 *******************************************************************************/  
-  pc_field_time constant fflu_common.st_name := 'Time';
-  pc_field_period constant fflu_common.st_name := 'Period';
-  pc_field_mars_period constant fflu_common.st_name := 'Mars Period';
-  pc_field_measure constant fflu_common.st_name := 'Measure';
-  pc_field_product constant fflu_common.st_name := 'Product';
-  pc_field_market constant fflu_common.st_name := 'Market';
-  pc_field_data_value constant fflu_common.st_name := 'Data Value';
-  pc_field_manufacturer constant fflu_common.st_name := 'Manufacturer';
+  pc_field_version constant fflu_common.st_name := 'Time';
   pc_field_brand constant fflu_common.st_name := 'Brand';
-  pc_field_animal_type constant fflu_common.st_name := 'Animal Type';
-  pc_field_department constant fflu_common.st_name := 'Department';
-  pc_field_category constant fflu_common.st_name := 'Category';
   pc_field_segment constant fflu_common.st_name := 'Segment';
-  pc_field_packtype constant fflu_common.st_name := 'Packtype';
-  pc_field_packsize constant fflu_common.st_name := 'Packsize';
-  pc_field_size constant fflu_common.st_name := 'Size';
-  pc_field_ean constant fflu_common.st_name := 'Ean';
+  pc_field_period constant fflu_common.st_name := 'Period';
+  pc_field_year constant fflu_common.st_name := 'Year';
+  pc_field_weeks_on_air constant fflu_common.st_name := 'Weeks on Air';
+  pc_field_4_weekly_reach constant fflu_common.st_name := '4 Weekly Reach';
 
 /*******************************************************************************
   Interface Sufix's
@@ -29,7 +19,7 @@ PACKAGE body LOGRWOD06_LOADER AS
 /*******************************************************************************
   Package Variables
 *******************************************************************************/  
-  pv_prev_mars_period logr_wod_sales_scan.mars_period%type;
+  pv_prev_version logr_wod_tv_activity.version%type;
   
 /*******************************************************************************
   NAME:      ON_START                                                     PUBLIC
@@ -37,26 +27,17 @@ PACKAGE body LOGRWOD06_LOADER AS
   procedure on_start is 
   begin
     -- Initialise any package processing variables.
-    pv_prev_mars_period := null;
+    pv_prev_version := null;
     -- Now initialise the data parsing wrapper.
     fflu_data.initialise(on_get_file_type,on_get_csv_qualifier,true,false);
     -- Now define the column structure
-    fflu_data.add_char_field_csv(pc_field_period,1,pc_field_time,null,14);
-    fflu_data.add_mars_date_field_csv(pc_field_mars_period,1,pc_field_time,'MARS_PERIOD','DD/MM/YY',6,8);
-    fflu_data.add_char_field_csv(pc_field_measure,2,pc_field_measure,null,100);
-    fflu_data.add_char_field_csv(pc_field_product,3,pc_field_product,null,100);
-    fflu_data.add_char_field_csv(pc_field_market,4,pc_field_market,null,100);
-    fflu_data.add_number_field_csv(pc_field_data_value,5,pc_field_data_value,null,-1000000,1000000,true);
-    fflu_data.add_char_field_csv(pc_field_manufacturer,6,pc_field_manufacturer,null,100);
-    fflu_data.add_char_field_csv(pc_field_brand,7,pc_field_brand,null,100);
-    fflu_data.add_char_field_csv(pc_field_animal_type,8,pc_field_animal_type,null,100);
-    fflu_data.add_char_field_csv(pc_field_department,9,pc_field_department,null,100);
-    fflu_data.add_char_field_csv(pc_field_category,10,pc_field_category,null,100);
-    fflu_data.add_char_field_csv(pc_field_segment,11,pc_field_segment,null,100,true);
-    fflu_data.add_char_field_csv(pc_field_packtype,12,pc_field_packtype,null,100,true);
-    fflu_data.add_char_field_csv(pc_field_packsize,13,pc_field_packsize,null,100,true);
-    fflu_data.add_number_field_csv(pc_field_size,14,pc_field_size,null,0,100000,false);
-    fflu_data.add_char_field_csv(pc_field_ean,15,pc_field_ean,null,100);
+    fflu_data.add_number_field_csv(pc_field_version,1,'Version',null,1900,999913,fflu_data.gc_not_allow_null);
+    fflu_data.add_char_field_csv(pc_field_brand,2,'Brand',null,100,fflu_data.gc_allow_null);
+    fflu_data.add_char_field_csv(pc_field_segment,3,'Segment',null,100,fflu_data.gc_allow_null);
+    fflu_data.add_number_field_csv(pc_field_period,4,'Period',null,1,13,fflu_data.gc_not_allow_null);
+    fflu_data.add_number_field_csv(pc_field_year,5,'Year',null,1900,9999,fflu_data.gc_not_allow_null);
+    fflu_data.add_number_field_csv(pc_field_weeks_on_air,6,'Weeks on Air',null,0,null,fflu_data.gc_allow_null);
+    fflu_data.add_number_field_csv(pc_field_4_weekly_reach,7,'4 Weekly Reach',null,0,null,fflu_data.gc_allow_null);
   exception 
     when others then 
       fflu_utils.log_interface_exception('On Start');
@@ -73,53 +54,35 @@ end on_start;
       -- Set an OK Tracking variable.
       v_ok := true;
       -- Check if this is the first data row and if the current mars period is set. 
-      if pv_prev_mars_period is null then 
-        pv_prev_mars_period := fflu_data.get_mars_date_field(pc_field_mars_period);
+      if pv_prev_version is null then 
+        pv_prev_version := fflu_data.get_number_field(pc_field_version);
         -- Clear out any previous data for this same mars period.
-        delete from logr_wod_sales_scan where mars_period = pv_prev_mars_period;
+        delete from logr_wod_tv_activity where version = pv_prev_version;
       else
         -- Now check that each supplied mars period is the same as the first one that was supplied in the file. 
-        if pv_prev_mars_period <> fflu_data.get_mars_date_field(pc_field_mars_period) then 
-          fflu_data.log_field_error(pc_field_mars_period,'Mars period was different to first period found in data file. [' || pv_prev_mars_period || '].');
+        if pv_prev_version <> fflu_data.get_number_field(pc_field_version) then 
+          fflu_data.log_field_error(pc_field_version,'Version was different to first version found in data file. [' || pv_prev_version || '].');
           v_ok := false;
         end if;
       end if;
       -- Now insert the logr sales scan data.
       if v_ok = true then 
-        insert into logr_wod_sales_scan (
-          period, 
-          mars_period, 
-          measure, 
-          product, 
-          market, 
-          data_value, 
-          MANUFACTURER,
-          BRAND,
-          ANIMAL_TYPE,
-          DEPARTMENT,
-          CATGRY,
-          SGMNT,
-          PACKTYPE,
-          PACKSIZE,
-          SZE_GRAMS,
-          EAN
+        insert into logr_wod_tv_activity (
+          version,
+          brand,
+          sgmnt,
+          period,
+          year,
+          weeks_on_air,
+          four_weekly_reach
         ) values (
-          fflu_data.get_char_field(pc_field_period), 
-          fflu_data.get_mars_date_field(pc_field_mars_period),
-          fflu_data.get_char_field(pc_field_measure),
-          fflu_data.get_char_field(pc_field_product),
-          fflu_data.get_char_field(pc_field_market),
-          fflu_data.get_number_field(pc_field_data_value),
-          fflu_data.get_char_field(pc_field_manufacturer),
+          fflu_data.get_number_field(pc_field_version), 
           fflu_data.get_char_field(pc_field_brand),
-          fflu_data.get_char_field(pc_field_animal_type),
-          fflu_data.get_char_field(pc_field_department),
-          fflu_data.get_char_field(pc_field_category),
           fflu_data.get_char_field(pc_field_segment),
-          fflu_data.get_char_field(pc_field_packtype),
-          fflu_data.get_char_field(pc_field_packsize),
-          fflu_data.get_number_field(pc_field_size),
-          fflu_data.get_char_field(pc_field_ean)
+          fflu_data.get_number_field(pc_field_period),
+          fflu_data.get_number_field(pc_field_year),
+          fflu_data.get_number_field(pc_field_weeks_on_air),
+          fflu_data.get_number_field(pc_field_4_weekly_reach)
         );
       end if;
     end if;
@@ -165,5 +128,5 @@ end on_start;
 
 -- Initialise this package.  
 begin
-  pv_prev_mars_period := null;
+  pv_prev_version := null;
 END LOGRWOD06_LOADER;
