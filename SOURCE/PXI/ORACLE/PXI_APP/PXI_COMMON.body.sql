7ab6e57ddab3afca8728038b43981b23925cfeb7
hornchr <hornchr>
2013-07-29 01:01:01 +0000

    Combined the various promax pxi development code into one package.  It still needs quite a bit of clean up.

--- SOURCE/PXI/ORACLE/PXI_APP/PXI_COMMON.body.sql
+++ SOURCE/PXI/ORACLE/PXI_APP/PXI_COMMON.body.sql
@@ -146,6 +146,374 @@ exception
     raise_application_error(-20000, substr('['||pv_package_name||'.date_format] : '||SQLERRM, 1, 4000));
 
 end date_format;
+
+
+
+FUNCTION format_cust_code (
+  i_cust_code IN VARCHAR2,
+  o_cust_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER IS
+
+  -- VARIABLE DECLARATIONS.
+  v_cust_code  VARCHAR2(10);
+  v_first_char VARCHAR2(1);
+
+BEGIN
+  -- Trim the inputted Customer Code.
+  v_cust_code := RTRIM(i_cust_code);
+
+  -- Return zero's if Customer Code is null.
+  IF v_cust_code IS NULL THEN
+    -- Return the value.
+    o_cust_code := '0000000000';
+  END IF;
+
+  -- Check whether the first character is a number.  If so, then left pad with zero's to
+  -- ten characters.  Otherwise right pad with spaces to ten characters.
+  v_first_char := SUBSTR(v_cust_code,1,1);
+  IF v_first_char >= '0' AND v_first_char <= '9' THEN
+    o_cust_code := LPAD(v_cust_code,10,'0');
+  ELSE
+    o_cust_code := RPAD(v_cust_code,10,' ');
+  END IF;
+
+  -- Now return a successful status.
+ RETURN 0; --constants.success;
+
+/*EXCEPTION
+  WHEN OTHERS THEN
+    o_result_msg :=
+      utils.create_error_msg('PDS_COMMON.FORMAT_CUST_CODE:',
+        'Unable to perform function.') ||
+      utils.create_params_str('Customer Code',i_cust_code) ||
+      utils.create_sql_err_msg();
+    RETURN constants.error;*/
+END format_cust_code;
+
+
+FUNCTION format_pmx_cust_code (
+  i_cust_code IN VARCHAR2,
+  o_pmx_cust_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER IS
+
+  -- VARIABLE DECLARATIONS.
+  v_pmx_cust_code  VARCHAR2(10);
+  v_first_char VARCHAR2(1);
+
+BEGIN
+  -- Trim the inputted Customer Code.
+  v_pmx_cust_code := LTRIM(TRIM(i_cust_code),'0');
+
+  -- Return zero's if Customer Code is null.
+  IF v_pmx_cust_code IS NULL THEN
+    -- Return the value.
+    o_pmx_cust_code := NULL;
+  END IF;
+
+  -- Right trim any spaces from the end of the Pmx Customer Code.
+  o_pmx_cust_code := RTRIM(v_pmx_cust_code,' ');
+
+  -- Now return a successful status.
+  RETURN 0; --constants.success;
+/*
+EXCEPTION
+  WHEN OTHERS THEN
+    o_result_msg :=
+      utils.create_error_msg('PDS_COMMON.FORMAT_PMX_CUST_CODE:',
+        'Unable to perform function.') ||
+      utils.create_params_str('Customer Code',i_cust_code) ||
+      utils.create_sql_err_msg();
+    RETURN constants.error;*/
+END format_pmx_cust_code;
+
+
+FUNCTION format_matl_code (
+  i_matl_code IN VARCHAR2,
+  o_matl_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER IS
+
+  -- VARIABLE DECLARATIONS.
+  v_matl_code VARCHAR2(18);
+  v_firstchar CHAR(1);
+
+BEGIN
+  -- Trim the inputted Material Code.
+  v_matl_code := LTRIM(i_matl_code);
+
+  -- Return zeros if Material Code is null.
+  IF v_matl_code IS NULL THEN
+    v_matl_code := '000000000000000000';
+  END IF;
+
+  -- Check whether the first character is a number. If so, then left pad with zero's to
+  -- eighteen characters. Otherwise right pad with white spaces to eighteen characters.
+  v_firstchar := SUBSTR(v_matl_code,1,1);
+  IF v_firstchar >= '0' AND v_firstchar <= '9' THEN
+    o_matl_code := LPAD(v_matl_code,18,'0');
+  ELSE
+    o_matl_code := RPAD(v_matl_code,18,' ');
+  END IF;
+
+  -- Now return a successful status.
+  RETURN 0; --constants.success;
+/*
+EXCEPTION
+  WHEN OTHERS THEN
+    o_result_msg :=
+      utils.create_error_msg('PDS_COMMON.FORMAT_MATL_CODE:',
+        'Unable to perform function.') ||
+      utils.create_params_str('Material Code',i_matl_code) ||
+      utils.create_sql_err_msg();
+    RETURN constants.error;*/
+END format_matl_code;
+
+
+FUNCTION format_pmx_matl_code (
+  i_matl_code IN VARCHAR2,
+  o_matl_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER IS
+
+  -- VARIABLE DECLARATIONS.
+  v_matl_code VARCHAR2(18);
+  v_firstchar CHAR(1);
+
+BEGIN
+  -- Trim the inputted Material Code.
+  v_matl_code := LTRIM(TRIM(i_matl_code), '0');
+  o_matl_code := v_matl_code;
+
+  -- Now return a successful status.
+  RETURN constants.success;
+/*
+EXCEPTION
+  WHEN OTHERS THEN
+    o_result_msg :=
+      utils.create_error_msg('PDS_COMMON.FORMAT_PMX_MATL_CODE:',
+        'Unable to perform function.') ||
+      utils.create_params_str('Material Code',i_matl_code) ||
+      utils.create_sql_err_msg();
+    RETURN constants.error;*/
+END format_pmx_matl_code;
+
+
+
+function lookup_matl_tdu_num (
+    i_matl_zrep_code    in  varchar2,
+    o_matl_tdu_code     out varchar2,
+    i_buy_start_date    in  date,
+    i_buy_end_date      in  date
+  ) return number is
+
+    -- VARIABLE DECLARATIONS.
+    v_result          number;
+    v_matl_zrep_code  varchar2(18);
+    v_matl_tdu_code   varchar2(18);
+      
+      
+    /*-*/ 
+    /* Lookup Current User
+    /*-*/   
+    cursor csr_matl_tdu is
+        select 
+            sales_organisation,
+            dstrbtn_channel,
+            sold_to_code,
+            zrep_material_code,
+            start_date,
+            end_date,
+            tdu_material_code
+        from
+            bds_refrnc_material_zrep t01,
+            bds_refrnc_material_tdu t02
+        where 
+            t01.condition_record_no = t02.condition_record_no
+            and t01.material_dtrmntn_type = 'Z001'
+            and t01.sales_organisation = '149'
+            and t01.zrep_material_code = v_matl_zrep_code 
+            and not (i_buy_end_date < start_date or i_buy_start_date > end_date)
+            order by start_date desc;
+    rcd_matl_tdu csr_matl_tdu%rowtype;
+
+BEGIN
+  
+    -- Format the ZREP Code  
+    v_result := format_matl_code(i_matl_zrep_code, v_matl_zrep_code);
+
+    /*-*/
+    /* Obtain the TDU number
+    /*-*/
+    open csr_matl_tdu;
+    fetch csr_matl_tdu into rcd_matl_tdu;
+    if csr_matl_tdu%notfound then
+       -- No result
+       o_matl_tdu_code := 0;
+    else
+       o_matl_tdu_code := rcd_matl_tdu.tdu_material_code;
+    end if;
+    close csr_matl_tdu;
+
+
+  -- Now return a successful status.
+  return 0; 
+
+end lookup_matl_tdu_num;
+
+
+function lookup_distbn_chnl_code (
+    i_cust_code         in  varchar2,
+    o_distbn_chnl_code     out varchar2
+  ) return number is
+
+    -- VARIABLE DECLARATIONS.
+    v_result     number;
+    v_cust_code  varchar2(18);
+      
+      
+    /*-*/ 
+    /* Lookup Current User
+    /*-*/   
+    cursor csr_distbn_chnl is
+        select 
+            customer_code, 
+            distbn_chnl_code
+        from 
+            bds_cust_sales_area t01
+        where 
+            t01.customer_code = i_cust_code;
+    rcd_distbn_chnl csr_distbn_chnl%rowtype;
+
+BEGIN
+  
+    -- Format the ZREP Code  
+    v_result := format_cust_code(i_cust_code, v_cust_code);
+
+    /*-*/
+    /* Obtain the TDU number
+    /*-*/
+    open csr_distbn_chnl;
+    fetch csr_distbn_chnl into rcd_distbn_chnl;
+    if csr_distbn_chnl%notfound then
+       -- No result
+       o_distbn_chnl_code := 10;
+    else
+       o_distbn_chnl_code := rcd_distbn_chnl.distbn_chnl_code;
+    end if;
+    close csr_distbn_chnl;
+
+
+  -- Now return a successful status.
+  return 0; 
+
+end lookup_distbn_chnl_code;
+
+
+function lookup_division_code (
+    i_matl_tdu_code     in  varchar2,
+    o_division_code     out varchar2
+  ) return number is
+
+    -- VARIABLE DECLARATIONS.
+    v_result          number;
+    v_matl_tdu_code  varchar2(18);
+      
+      
+    /*-*/ 
+    /* Lookup Current User
+    /*-*/   
+    cursor csr_matl_division is
+        select 
+            sap_material_code,
+            material_division
+        from
+            bds_material_hdr t01
+        where
+            t01.sap_material_code = v_matl_tdu_code;
+    rcd_matl_division csr_matl_division%rowtype;
+
+BEGIN
+  
+    -- Format the TDU Code  
+    v_result := format_matl_code(i_matl_tdu_code, v_matl_tdu_code);
+
+    /*-*/
+    /* Obtain the Division number
+    /*-*/
+    open csr_matl_division;
+    fetch csr_matl_division into rcd_matl_division;
+    if csr_matl_division%notfound then
+       -- No result
+       o_division_code := 0;
+    else
+       o_division_code := rcd_matl_division.material_division;
+    end if;
+    close csr_matl_division;
+
+
+  -- Now return a successful status.
+  return 0; 
+
+end lookup_division_code;
+
+
+function lookup_plant_code (
+    i_matl_tdu_code     in  varchar2,
+    o_plant_code        out varchar2
+  ) return number is
+
+    -- VARIABLE DECLARATIONS.
+    v_result          number;
+    v_matl_tdu_code  varchar2(18);
+      
+      
+    /*-*/ 
+    /* Lookup Current User
+    /*-*/   
+    cursor csr_matl_plant is
+        select 
+            sap_material_code,
+            plant_code 
+        from
+            bds_material_plant_hdr t01
+        where
+            t01.sap_material_code = v_matl_tdu_code
+            AND t01.plant_code != 'AU10' -- Note: No sales occur from this plant.
+            AND SUBSTR(t01.plant_code, 1, 2) = 'NZ'
+        order by 
+            t01.plant_specific_status_valid desc;
+    rcd_matl_plant csr_matl_plant%rowtype;
+
+BEGIN
+  
+    -- Format the TDU Code  
+    v_result := format_matl_code(i_matl_tdu_code, v_matl_tdu_code);
+
+    /*-*/
+    /* Obtain the Division number
+    /*-*/
+    open csr_matl_plant;
+    fetch csr_matl_plant into rcd_matl_plant;
+    if csr_matl_plant%notfound then
+       -- No result
+       o_plant_code := 'NZ01';
+    else
+       o_plant_code := rcd_matl_plant.plant_code;
+    end if;
+    close csr_matl_plant;
+
+
+  -- Now return a successful status.
+  return 0; 
+
+end lookup_plant_code;
+
 --------------------------------------------------------------------------------
 end pxi_common;
 /
--- SOURCE/PXI/ORACLE/PXI_APP/PXI_COMMON.sql
+++ SOURCE/PXI/ORACLE/PXI_APP/PXI_COMMON.sql
@@ -1,5 +1,64 @@
 create or replace package pxi_common as
 
+
+/*******************************************************************************
+  NAME:      format_cust_code
+  PURPOSE:   This function formats Customer Codes by left padding with '0' to
+             10 characters with numeric Customer Codes.  If the Customer Code
+             is not numeric then it is right padded with spaces to 10 characters.
+             This is the required format when extracting to SAP.
+
+********************************************************************************/
+FUNCTION format_cust_code (
+  i_cust_code IN VARCHAR2,
+  o_cust_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER;
+
+/*******************************************************************************
+  NAME:      format_pmx_cust_code
+  PURPOSE:   This function formats Promax Customer Codes by left trimming '0's from
+             the passed Customer Code.
+
+********************************************************************************/
+FUNCTION format_pmx_cust_code (
+  i_cust_code IN VARCHAR2,
+  o_pmx_cust_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER;
+
+/*******************************************************************************
+  NAME:      format_matl_code
+  PURPOSE:   Materials have leading zeroes if they are numeric, otherwise the
+             field is left justified with spaces padding (on the right). The
+             width returned is 18 characters.
+
+********************************************************************************/
+FUNCTION format_matl_code (
+  i_matl_code IN VARCHAR2,
+  o_matl_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER;
+  
+
+/*******************************************************************************
+  NAME:      format_pmx_matl_code
+  PURPOSE:   Material Codes have leading zeroes if they are numeric. These leading
+             zeroes need to be trimmed if they are to be inserted into Promax.
+
+********************************************************************************/
+FUNCTION format_pmx_matl_code (
+  i_matl_code IN VARCHAR2,
+  o_matl_code OUT VARCHAR2
+  --i_log_level IN NUMBER,
+  --o_result_msg OUT VARCHAR2
+  ) RETURN NUMBER;
+
+
+
 function char_format(i_value in varchar2, i_length in number, i_format_type in number, i_value_is_nullable in number) return varchar2;
 function numb_format(i_value in number, i_format in varchar2, i_value_is_nullable in number) return varchar2;
 function date_format(i_value in date, i_format in varchar2, i_value_is_nullable in number) return varchar2;
@@ -12,6 +71,54 @@ function format_type_trim return number;
 function format_type_ltrim return number;
 function format_type_rtrim return number;
 function format_type_ltrim_zeros return number;
+
+
+
+
+/*******************************************************************************
+  NAME:      lookup_matl_tdu_num
+  PURPOSE:   This function looks up the material TDU Number.
+
+********************************************************************************/
+function lookup_matl_tdu_num (
+    i_matl_zrep_code    in  varchar2,
+    o_matl_tdu_code     out varchar2,
+    i_buy_start_date    in  date,
+    i_buy_end_date      in  date
+  ) return number;
+  
+  
+/*******************************************************************************
+  NAME:      lookup_distbn_chnl_code
+  PURPOSE:   This function looks up the distribution channel code. 
+  
+********************************************************************************/
+function lookup_distbn_chnl_code (
+    i_cust_code         in  varchar2,
+    o_distbn_chnl_code     out varchar2
+  ) return number;
+
+
+/*******************************************************************************
+  NAME:      lookup_division_code
+  PURPOSE:   This function looks up the division code. 
+  
+********************************************************************************/
+function lookup_division_code (
+    i_matl_tdu_code     in  varchar2,
+    o_division_code     out varchar2
+  ) return number;
+
+
+/*******************************************************************************
+  NAME:      lookup_plant_code
+  PURPOSE:   This function looks up the plant code. 
+  
+********************************************************************************/
+function lookup_plant_code (
+    i_matl_tdu_code     in  varchar2,
+    o_plant_code        out varchar2
+  ) return number;
 
 end pxi_common;
 /
