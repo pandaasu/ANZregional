791237dcc05ee9612f742d0680ecf392c3372833
chambmal <chambmal>
2013-07-29 10:54:52 +0000

    Format and RAISE ..

--- SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.body.sql
+++ SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.body.sql
@@ -22,51 +22,44 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
       /* Local cursors
       /*-*/
       cursor csr_input is
-        --======================================================================
-        select 
-        ------------------------------------------------------------------------
-        -- FORMAT OUTPUT
-        ------------------------------------------------------------------------
+        select
           pxi_common.char_format('306001', 6, pxi_common.format_type_none, pxi_common.is_nullable) || -- CONSTANT '306001' -> ICRecordType
-          pxi_common.char_format('149', 10, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- CONSTANT '149' -> PXCompanyCode
-          pxi_common.char_format(hdr_division_code, 10, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- hdr_division_code -> PXDivisionCode
+          pxi_common.char_format('149', 3, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- CONSTANT '149' -> PXCompanyCode
+          pxi_common.char_format(hdr_division_code, 3, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- hdr_division_code -> PXDivisionCode
           pxi_common.char_format(sold_to_cust_code, 20, pxi_common.format_type_ltrim_zeros, pxi_common.is_not_nullable) || -- sold_to_cust_code -> CustomerNumber
           pxi_common.char_format(billing_doc_num, 10, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- billing_doc_num -> InvoiceNumber
           pxi_common.char_format(billing_doc_line_num, 6, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- billing_doc_line_num -> InvoiceLineNumber
           pxi_common.char_format(matl_entd, 18, pxi_common.format_type_ltrim_zeros, pxi_common.is_not_nullable) || -- matl_entd -> Material
-          pxi_common.date_format(order_eff_date, 'dd/mm/yyyy', pxi_common.is_not_nullable) || -- order_eff_date -> OrderDate
-          pxi_common.date_format(billing_eff_date, 'dd/mm/yyyy', pxi_common.is_not_nullable) || -- billing_eff_date -> InvoiceDate
+          pxi_common.date_format(order_eff_date, 'yyyymmdd', pxi_common.is_not_nullable) || -- order_eff_date -> OrderDate
+          pxi_common.date_format(billing_eff_date, 'yyyymmdd', pxi_common.is_not_nullable) || -- billing_eff_date -> InvoiceDate
           pxi_common.numb_format(billed_qty_base_uom, 'S9999999999990.00', pxi_common.is_not_nullable) || -- billed_qty_base_uom -> QuantityInvoiced
           pxi_common.numb_format(billed_gsv, 'S999999990.00', pxi_common.is_not_nullable) || -- billed_gsv -> GrossAmount
           pxi_common.char_format(doc_currcy_code, 5, pxi_common.format_type_none, pxi_common.is_nullable) -- doc_currcy_code -> Currency
-        ------------------------------------------------------------------------
         from (
-        ------------------------------------------------------------------------
-        -- SQL
-        ------------------------------------------------------------------------
-          select 
+          select
             t1.hdr_division_code,
             t1.company_code,
             t1.sold_to_cust_code,
-            t1.billing_doc_num, 
-            t1.billing_doc_line_num, 
+            t1.billing_doc_num,
+            t1.billing_doc_line_num,
             t1.matl_entd,
             t2.order_eff_date,
             t1.billing_eff_date,
-            t1.billed_qty_base_uom, 
+            t1.billed_qty_base_uom,
             t1.billed_gsv,
             t1.doc_currcy_code
-          from 
+          from
             dw_sales_base@db1270p_promax_testing t1,
             dw_order_base@db1270p_promax_testing t2
-          where 
-            t1.company_code = 149 
-            and t1.creatn_date = trunc(i_creation_date) 
-            and t1.order_doc_num = t2.order_doc_num (+) 
+          where
+            t1.company_code = 149
+            and t1.creatn_date = trunc(i_creation_date)
+            and t1.order_doc_num = t2.order_doc_num (+)
             and t1.order_doc_line_num = t2.order_doc_line_num (+)
-        ------------------------------------------------------------------------
+            -- not null check added to accommodate new restrictions on output format
+            and t1.matl_entd is not null 
+            and t2.order_eff_date is not null        
         );
-        --======================================================================
 
    /*-------------*/
    /* Begin block */
@@ -119,6 +112,7 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
             lics_outbound_loader.add_exception(substr(SQLERRM, 1, 512));
             lics_outbound_loader.finalise_interface;
          end if;
+         raise;
 
    /*-------------*/
    /* End routine */
