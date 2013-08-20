f289f95d342ed769286cece1662b9ead21bfd269
hornchr <hornchr>
2013-08-20 04:28:31 +0000

    Updated package to be more generic and handle other companies and divisions.

--- SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.body.sql
+++ SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.body.sql
@@ -1,40 +1,38 @@
 create or replace 
-PACKAGE BODY          PXIPMX07_EXTRACT as
+PACKAGE BODY PXIPMX07_EXTRACT as
 
-   /*-*/
-   /* Private exceptions
-   /*-*/
-   application_exception exception;
-   pragma exception_init(application_exception, -20000);
+/*******************************************************************************
+  Package Cosntants
+*******************************************************************************/
+  pc_package_name constant pxi_common.st_package_name := 'PXIPMX07_EXTRACT';
 
-   /***********************************************/
-   /* This procedure performs the execute routine */
-   /***********************************************/
-   procedure execute(i_creation_date in date default sysdate-1) is
-
-      /*-*/
-      /* Local definitions
-      /*-*/
-      var_instance number(15,0);
-      var_data varchar2(4000);
-
-      /*-*/
-      /* Local cursors
-      /*-*/
-      cursor csr_input is
+/*******************************************************************************
+  NAME:  EXECUTE                                                          PUBLIC
+*******************************************************************************/
+   procedure execute(
+     i_pmx_company in pxi_common.st_company default null,
+     i_pmx_division in pxi_common.st_promax_division default null, 
+     i_creation_date in date default sysdate-1) is
+     -- Variables     
+     v_instance number(15,0);
+     v_data pxi_common.st_data;
+     
+     -- Extract Cursor.
+     cursor csr_input is
         select
-          pxi_common.char_format('306001', 6, pxi_common.format_type_none, pxi_common.is_nullable) || -- CONSTANT '306001' -> ICRecordType
-          pxi_common.char_format('149', 3, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- CONSTANT '149' -> PXCompanyCode
-          pxi_common.char_format(hdr_division_code, 3, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- hdr_division_code -> PXDivisionCode
-          pxi_common.char_format(sold_to_cust_code, 20, pxi_common.format_type_ltrim_zeros, pxi_common.is_not_nullable) || -- sold_to_cust_code -> CustomerNumber
-          pxi_common.char_format(billing_doc_num, 10, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- billing_doc_num -> InvoiceNumber
-          pxi_common.char_format(billing_doc_line_num, 6, pxi_common.format_type_none, pxi_common.is_not_nullable) || -- billing_doc_line_num -> InvoiceLineNumber
-          pxi_common.char_format(matl_entd, 18, pxi_common.format_type_ltrim_zeros, pxi_common.is_not_nullable) || -- matl_entd -> Material
-          pxi_common.date_format(order_eff_date, 'yyyymmdd', pxi_common.is_not_nullable) || -- order_eff_date -> OrderDate
-          pxi_common.date_format(billing_eff_date, 'yyyymmdd', pxi_common.is_not_nullable) || -- billing_eff_date -> InvoiceDate
-          pxi_common.numb_format(billed_qty_base_uom, 'S9999999999990.00', pxi_common.is_not_nullable) || -- billed_qty_base_uom -> QuantityInvoiced
-          pxi_common.numb_format(billed_gsv, 'S999999990.00', pxi_common.is_not_nullable) || -- billed_gsv -> GrossAmount
-          pxi_common.char_format(doc_currcy_code, 5, pxi_common.format_type_none, pxi_common.is_nullable) -- doc_currcy_code -> Currency
+          pxi_common.char_format('306001', 6, pxi_common.fc_format_type_none, pxi_common.fc_is_nullable) || -- CONSTANT '306001' -> ICRecordType
+          pxi_common.char_format(company_code, 3, pxi_common.fc_format_type_none, pxi_common.fc_is_not_nullable) || -- CONSTANT '149' -> PXCompanyCode
+          -- TODO Need to confirm if this should be t4.promax_division instead of hdr_division_code
+          pxi_common.char_format(hdr_division_code, 3, pxi_common.fc_format_type_none, pxi_common.fc_is_not_nullable) || -- hdr_division_code -> PXDivisionCode
+          pxi_common.char_format(sold_to_cust_code, 20, pxi_common.fc_format_type_ltrim_zeros, pxi_common.fc_is_not_nullable) || -- sold_to_cust_code -> CustomerNumber
+          pxi_common.char_format(billing_doc_num, 10, pxi_common.fc_format_type_none, pxi_common.fc_is_not_nullable) || -- billing_doc_num -> InvoiceNumber
+          pxi_common.char_format(billing_doc_line_num, 6, pxi_common.fc_format_type_none, pxi_common.fc_is_not_nullable) || -- billing_doc_line_num -> InvoiceLineNumber
+          pxi_common.char_format(rep_item, 18, pxi_common.fc_format_type_ltrim_zeros, pxi_common.fc_is_not_nullable) || -- matl_entd -> Material
+          pxi_common.date_format(order_eff_date, 'yyyymmdd', pxi_common.fc_is_not_nullable) || -- order_eff_date -> OrderDate
+          pxi_common.date_format(billing_eff_date, 'yyyymmdd', pxi_common.fc_is_not_nullable) || -- billing_eff_date -> InvoiceDate
+          pxi_common.numb_format(billed_qty_base_uom, 'S9999999999990.00', pxi_common.fc_is_not_nullable) || -- billed_qty_base_uom -> QuantityInvoiced
+          pxi_common.numb_format(billed_gsv, 'S999999990.00', pxi_common.fc_is_not_nullable) || -- billed_gsv -> GrossAmount
+          pxi_common.char_format(doc_currcy_code, 5, pxi_common.fc_format_type_none, pxi_common.fc_is_nullable) -- doc_currcy_code -> Currency
         from (
           select
             t1.hdr_division_code,
@@ -42,7 +40,7 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
             t1.sold_to_cust_code,
             t1.billing_doc_num,
             t1.billing_doc_line_num,
-            t1.matl_entd,
+            t3.rep_item,
             t2.order_eff_date,
             t1.billing_eff_date,
             t1.billed_qty_base_uom,
@@ -50,15 +48,23 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
             t1.doc_currcy_code
           from
             dw_sales_base@db1270p_promax_testing t1,
-            dw_order_base@db1270p_promax_testing t2
+            dw_order_base@db1270p_promax_testing t2,
+            matl_dim@db1270p_promax_testing t3,
+            table(pxi_common.promax_config(i_pmx_company,i_pmx_division)) t4
           where
-            t1.company_code = 149
-            and t1.creatn_date = trunc(i_creation_date)
+            -- Join to promax configuration table.
+            t1.company_code = t4.promax_company 
+            and ((t1.company_code = pxi_common.gc_australia and t1.hdr_division_code = t4.promax_division) or (t1.company_code = pxi_common.gc_new_zealand))
+            -- Extract yesterdays data by default, otherwise extract a whole range of data. for history since 2012.
+            and t1.company_code = t2.company_code (+)
+            and t1.creatn_date between nvl(trunc(i_creation_date),to_date('01/01/2012','DD/MM/YYYY')) and nvl(trunc(i_creation_date),trunc(sysdate-1))
             and t1.order_doc_num = t2.order_doc_num (+)
             and t1.order_doc_line_num = t2.order_doc_line_num (+)
-            -- not null check added to accommodate new restrictions on output format
-            and t1.matl_entd is not null 
-            and t2.order_eff_date is not null        
+            -- Now join to the material zrep detail
+            and t1.matl_code = t3.matl_code
+            -- Not null check added to accommodate new restrictions on output format
+            and t1.matl_entd is not null
+            and t2.order_eff_date is not null
         );
 
    /*-------------*/
@@ -71,7 +77,7 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
       /*-*/
       open csr_input;
       loop
-         fetch csr_input into var_data;
+         fetch csr_input into v_data;
          if csr_input%notfound then
             exit;
          end if;
@@ -80,13 +86,13 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
          /* Create the new interface when required
          /*-*/
          if lics_outbound_loader.is_created = false then
-            var_instance := lics_outbound_loader.create_interface('PXIPMX07');
+            v_instance := lics_outbound_loader.create_interface('PXIPMX07');
          end if;
 
          /*-*/
          /* Append the interface data
          /*-*/
-         lics_outbound_loader.append_data(var_data);
+         lics_outbound_loader.append_data(v_data);
 
       end loop;
       close csr_input;
@@ -98,26 +104,14 @@ PACKAGE BODY          PXIPMX07_EXTRACT as
          lics_outbound_loader.finalise_interface;
       end if;
 
-   /*-------------------*/
-   /* Exception handler */
-   /*-------------------*/
    exception
-
-      /**/
-      /* Exception trap
-      /**/
-      when others then
-         rollback;
-         if lics_outbound_loader.is_created = true then
-            lics_outbound_loader.add_exception(substr(SQLERRM, 1, 512));
-            lics_outbound_loader.finalise_interface;
-         end if;
-         raise;
-
-   /*-------------*/
-   /* End routine */
-   /*-------------*/
+     when others then
+       rollback;
+       if lics_outbound_loader.is_created = true then
+         lics_outbound_loader.add_exception(substr(SQLERRM, 1, 512));
+         lics_outbound_loader.finalise_interface;
+       end if;
+       pxi_common.reraise_promax_exception(pc_package_name,'EXECUTE');
    end execute;
 
-end PXIPMX07_EXTRACT;
-/
+end PXIPMX07_EXTRACT; 
--- SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.sql
+++ SOURCE/PXI/ORACLE/DDS_APP/PXIPMX07_EXTRACT.sql
@@ -11,19 +11,37 @@ PACKAGE          PXIPMX07_EXTRACT as
 
  Description
  -----------
- VENUS -> LADS (Pass Through) -> Promax PX - Sales Data - PX Interface 306
+    VENUS -> LADS (Pass Through) -> Promax PX - Sales Data - PX Interface 306
 
- This interface selects sales data for the specific creation date and company 
+ This interface selects sales data for the specific creation date and company
  149 and extracts that data and then onsends it to Lads for Pass through.
 
  Date          Author                Description
  ------------  --------------------  -----------
- 23/07/2013    Chris Horn            Created.
- 25/07/2013    Mal Chambeyron        Formatted SQL Output         
+ 2013-07-23    Chris Horn            Created.
+ 2013-07-25    Mal Chambeyron        Formatted SQL Output
+ 2013-08-20    Chris Horn            Updated with revision.
  
+
 *******************************************************************************/
 
-   procedure execute(i_creation_date in date default sysdate-1);
+/*******************************************************************************
+  NAME:      EXECUTE                                                      PUBLIC
+  PURPOSE:   This function creates an extract of sales data for promax.
+             It defaults to all available promax companies and divisions and
+             for sales from yesterday.  If null is supplied as the creation data
+             it will generate all sales history from the hard coded sales 
+             history date.
+
+  REVISIONS:
+  Ver   Date       Author               Description
+  ----- ---------- -------------------- ----------------------------------------
+  1.1   2013-07-30 Chris Horn           Created.
+
+*******************************************************************************/
+   procedure execute(
+     i_pmx_company in pxi_common.st_company default null,
+     i_pmx_division in pxi_common.st_promax_division default null, 
+     i_creation_date in date default sysdate-1);
 
 end PXIPMX07_EXTRACT;
-/
