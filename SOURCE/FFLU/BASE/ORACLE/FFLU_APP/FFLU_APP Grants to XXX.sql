1e7879d0f098a56a384968909b4fd659051037f2
hornchr <hornchr>
2013-09-05 23:40:06 +0000

    Renamed file and made generic.

--- SOURCE/FFLU/BASE/ORACLE/FFLU_APP/FFLU_APP Grants to XXX.sql
+++ SOURCE/FFLU/BASE/ORACLE/FFLU_APP/FFLU_APP Grants to XXX.sql
@@ -1,10 +1,11 @@
 -- This script should be run within FFLU_APP to grant execute access to the 
 -- approperiate packages for other app schemas to create interfaces.
 
-grant execute on fflu_common to mblt_app;
-grant execute on fflu_data to mblt_app;
-grant execute on fflu_utils to mblt_app;  
+grant execute on fflu_common to xxx_app;
+grant execute on fflu_data to xxx_app;
+grant execute on fflu_utils to xx_app;  
 
+-- These can be run in the xxx schema.
 create or replace synonym fflu_common for fflu_app.fflu_common;
 create or replace synonym fflu_data for fflu_app.fflu_data;
 create or replace synonym fflu_utils for fflu_app.fflu_utils;
