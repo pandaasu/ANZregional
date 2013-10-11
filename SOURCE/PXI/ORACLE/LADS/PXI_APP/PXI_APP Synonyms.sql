8a21d571e92e2b5a0c459ae5d34fe8362f33aa17
hornchr <hornchr>
2013-10-11 06:21:43 +0000

    Update the synonyms script.

--- SOURCE/PXI/ORACLE/LADS/PXI_APP/PXI_APP Synonyms.sql
+++ SOURCE/PXI/ORACLE/LADS/PXI_APP/PXI_APP Synonyms.sql
@@ -1,8 +1,10 @@
 -- Create a local synonyms as public ones are not always allowed.
-create or replace synonym pmx_matl_dtrmntn for "PXI"."PMX_MATL_DTRMNTN";
+create or replace synonym pmx_matl_dtrmntn for PXI.PMX_MATL_DTRMNTN;
 create or replace synonym pmx_359_promotions_seq for pxi.pmx_359_promotions_seq;
 create or replace synonym pmx_359_promotions for pxi.pmx_359_promotions;
-create or replace synonym "PMX_ZREP_MATERIALS" for "PXI"."PMX_ZREP_MATERIALS";
+create or replace synonym PMX_ZREP_MATERIALS for PXI.PMX_ZREP_MATERIALS;
 create or replace synonym pmx_prom_config for pxi.pmx_prom_config;
-create or replace synonym "PMX_MATL_TDU_TO_RSU" for "PXI"."PMX_MATL_TDU_TO_RSU";
+create or replace synonym PMX_MATL_TDU_TO_RSU for PXI.PMX_MATL_TDU_TO_RSU;
 create or replace synonym PMX_MATL_HIST for pxi.PMX_MATL_HIST;
+create or replace synonym PMX_AR_CLAIMS for pxi.PMX_AR_CLAIMS;
+create or replace synonym PMX_AR_CLAIMS_DUPS for pxi.PMX_AR_CLAIMS_DUPS;
