079e6fd894f01e6fa262cb24e7e375dfc1bb7baf
hornchr <hornchr>
2013-11-23 03:24:53 +0000

    Included the missing synonym for the pricing condition information.

--- SOURCE/PXI/ORACLE/LADS/PXI_APP/PXI_APP Synonyms.sql
+++ SOURCE/PXI/ORACLE/LADS/PXI_APP/PXI_APP Synonyms.sql
@@ -4,6 +4,7 @@ create or replace synonym pmx_359_promotions_seq for pxi.pmx_359_promotions_seq;
 create or replace synonym pmx_359_promotions for pxi.pmx_359_promotions;
 create or replace synonym PMX_ZREP_MATERIALS for PXI.PMX_ZREP_MATERIALS;
 create or replace synonym pmx_prom_config for pxi.pmx_prom_config;
+create or replace synonym pmx_price_conditions for pxi.pmx_price_conditions;
 create or replace synonym PMX_MATL_TDU_TO_RSU for PXI.PMX_MATL_TDU_TO_RSU;
 create or replace synonym PMX_MATL_HIST for pxi.PMX_MATL_HIST;
 create or replace synonym PMX_AR_CLAIMS for pxi.PMX_AR_CLAIMS;
