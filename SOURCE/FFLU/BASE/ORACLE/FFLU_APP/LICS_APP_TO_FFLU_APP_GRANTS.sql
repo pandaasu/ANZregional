49a584f345178c4ce22935e9e98420695762e82c
keontre <keontre>
2014-06-20 04:45:45 +0000

    KEONTRE - Added synonym for fflu_common to allow polling jobs to be run from LICS_APP without including the schema name.

--- SOURCE/FFLU/BASE/ORACLE/FFLU_APP/LICS_APP_TO_FFLU_APP_GRANTS.sql
+++ SOURCE/FFLU/BASE/ORACLE/FFLU_APP/LICS_APP_TO_FFLU_APP_GRANTS.sql
@@ -1,3 +1,6 @@
 -- This is the grant script required to be run on LICS_APP schema for the 
 -- purpose of the Flat File Loating Utility.
 grant execute on lics_pipe to fflu_app;
+
+-- Add synonym for fflu_common for running tasks from LICS
+create or replace synonym fflu_common for fflu_app.fflu_common;
