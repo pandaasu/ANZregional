00bfe545ce5f57dd73c484f360a77f7f50e2ce72
hornchr <hornchr>
2014-08-24 22:13:47 +0000

    Commit initial forecast.

---
+++ SOURCE/IPS/BASE/ORACLE/DF_APP/DFNAPO01_EXTRACT.body.sql
@@ -0,0 +1,267 @@
+create or replace package body dfnapo01_extract as
+
+/*******************************************************************************
+  Package Cosntants
+*******************************************************************************/
+  pc_package_name constant pxi_common.st_package_name := 'DFNAPO01_EXTRACT';
+  pc_interface_name constant pxi_common.st_interface_name := 'DFNAPO01';
+
+/*******************************************************************************
+  NAME:  FORECAST_MOE                                                     PUBLIC
+*******************************************************************************/
+   function forecast_moe(
+    i_fcst_id in common.st_id
+   ) return common.st_code is
+   v_moe_code pxi_common.st_moe_code;
+   begin
+    -- Get moe code for the supplied forecast id
+    v_moe_code := null;
+    begin
+      select moe_code into v_moe_code from fcst where fcst_id = i_fcst_id;
+    exception
+      when no_data_found then
+        null; -- Ignore
+    end;
+    return v_moe_code;
+  exception
+    when others then
+      raise;
+  end forecast_moe;
+
+/*******************************************************************************
+  NAME:  FIRST_DATE_MARS_WEEK                                             PUBLIC
+*******************************************************************************/
+   function first_date_mars_week(
+    i_mars_week in common.st_count
+   ) return date is
+   v_date date;
+   begin
+    -- Get first date of casting week for forecast provided
+    begin
+      select min(calendar_date) into v_date
+      from mars_date
+      where mars_week = i_mars_week;
+    exception
+      when no_data_found then
+        null; -- Ignore
+    end;
+    return v_date;
+  exception
+    when others then
+      raise;
+  end first_date_mars_week;
+
+/*******************************************************************************
+  NAME: GET_FORECAST                                                      PUBLIC
+*******************************************************************************/
+  function get_forecast (
+    i_fcst_id in common.st_id
+  ) return tt_forecast pipelined is
+  begin
+    -- Fetch each row of the defined query back out as a pipelined table function.
+    for rv_row in (
+      select
+        t10.fcst_id,
+        t10.moe_code,
+        t10.dmnd_grp_code,
+        t10.mars_week,
+        -- Output Fields
+        t10.tdu_matl_code,
+        ( select 
+            t0.plant_code 
+          from 
+            plng_srce_plant_xref t0
+          where 
+            t0.plng_srce_code = 
+              ( select 
+                  t00.plng_srce_code 
+                from 
+                  matl_fg_clssfctn t00 
+                where 
+                  t00.matl_code = reference_functions.full_matl_code(t10.tdu_matl_code)
+              )
+        ) as plant_code,
+        first_date_mars_week(t10.mars_week) as start_date,
+        t10.qty
+      from
+        (select
+          t1.fcst_id,
+          t1.moe_code,
+          t4.dmnd_grp_code,
+          t2.mars_week,
+          -- Output Fields
+          t2.tdu as tdu_matl_code,
+          round(sum(t2.qty_in_base_uom)) as qty
+        from
+          fcst t1,
+          dmnd_data t2,
+          dmnd_grp_org t3,
+          dmnd_grp t4
+        where 
+          -- Base Joines
+          t1.fcst_id = t2.fcst_id 
+          and t2.dmnd_grp_org_id = t3.dmnd_grp_org_id
+          and t3.dmnd_grp_id = t4.dmnd_grp_id 
+          -- Filter Predicates
+          and t1.fcst_id = i_fcst_id -- Limit to fcst_id
+          and t3.acct_assign_id in (
+            select acct_assign_id
+            from dmnd_acct_assign
+            where acct_assign_code = demand_forecast.gc_acct_assgnmnt_domestic
+          )
+        group by
+          t1.fcst_id,
+          t1.moe_code,
+          t4.dmnd_grp_code,
+          t2.mars_week,
+          t2.tdu
+        having
+          sum(t2.qty_in_base_uom) > 0 -- Filter unnecessary rows
+      ) t10
+    )
+    loop
+      pipe row(rv_row);
+    end loop;
+  exception
+    when others then
+      raise;
+  end get_forecast;
+
+/*******************************************************************************
+  NAME:  EXECUTE                                                          PUBLIC
+*******************************************************************************/
+   procedure execute(
+    i_fcst_id in common.st_id
+   ) is
+    v_moe_code pxi_common.st_moe_code;
+    v_interface_name_with_suffix pxi_common.st_interface_name;
+    v_instance number(15,0);
+    v_promax_rows_loaded number(20,0);
+    v_forecast_count number(1,0);
+  begin
+    -- Start Logging ..
+    lics_logging.start_log('DF to Apolllo Supply','DF_TO_APOLLO_SUPPLY_'||i_fcst_id);
+    -- Check for Forecast
+    select count(1) into v_forecast_count
+    from df.fcst
+    where fcst_id = i_fcst_id
+    and forecast_type in ('DRAFT','FCST');
+    if v_forecast_count = 0 then
+      lics_logging.write_log('NOTHING TO DO : Forecast ['||i_fcst_id||'] Either NOT FOUND : Forecast Type [DRAFT|FCST].');
+      lics_logging.end_log;
+      return;
+    end if;
+
+    -- Do NOTHING for MOE's Other than Petcare [0196], for Execute
+    v_moe_code := forecast_moe(i_fcst_id); -- assign so we don't need to lookup twice
+    if v_moe_code != pxi_common.fc_moe_pet then
+      lics_logging.write_log('NOTHING TO DO : Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'] : Processing Limited to MOE ['||pxi_common.fc_moe_pet||'].');
+      lics_logging.end_log;
+      return;
+    end if;
+
+    -- Check If Forecase Already has Promax PX Estimates Loaded
+    select count(1) into v_promax_rows_loaded
+    from df.dmnd_data
+    where fcst_id = i_fcst_id
+    and type in ('B','U','P');
+    -- No Need to Continue If Forecast Already has Promax PX Estimates Loaded
+    if v_promax_rows_loaded > 0 then
+      lics_logging.write_log('NOTHING TO DO : Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'] : Already has Promax PX Estimates Loaded, Types [B|U|P].');
+      lics_logging.end_log;
+      return;
+    end if;
+
+    -- request lock (on interface)
+    lics_logging.write_log('INFO : Request Interface Lock ['||pc_interface_name||'] for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+    begin
+      lics_locking.request(pc_interface_name);
+    exception
+      when others then
+        lics_logging.write_log('ERROR : Unable to Obtain Interface Lock ['||pc_interface_name||'] for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+        lics_logging.end_log;
+        pxi_common.raise_promax_error(pc_package_name,'EXECUTE',substr('Unable to obtain interface lock ['||pc_interface_name||'] - '||sqlerrm, 1, 4000));
+    end;
+
+    -- Now determine the interface name to use for this file.
+    v_interface_name_with_suffix := null;
+    case v_moe_code
+      when pxi_common.fc_moe_nz then
+        v_interface_name_with_suffix := pc_interface_name || '.' || pxi_common.fc_interface_nz;
+      when pxi_common.fc_moe_pet then
+        v_interface_name_with_suffix := pc_interface_name || '.' || pxi_common.fc_interface_pet;
+      when pxi_common.fc_moe_food then
+        v_interface_name_with_suffix := pc_interface_name || '.' || pxi_common.fc_interface_food;
+      when pxi_common.fc_moe_snack then
+        v_interface_name_with_suffix := pc_interface_name || '.' || pxi_common.fc_interface_snack;
+      else
+        lics_logging.write_log('ERROR : Unknown MOE [' || v_moe_code || '] for Forecast [' || i_fcst_id || '].');
+        lics_logging.end_log;
+        pxi_common.raise_promax_error(pc_package_name,'EXECUTE','Unknown MOE [' || v_moe_code || '] for Forecast [' || i_fcst_id || '].');
+    end case;
+
+    -- Ensure 335DMND temporary table is empty
+    lics_logging.write_log('INFO : Ensure 355DMND Temporary Table [px_355dmnd_history_temp] is Empty.');
+    delete from px_355dmnd_history_temp;
+
+    lics_logging.write_log('INFO : Create 355DMND Records for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+
+    for rv_row in (
+      select
+        output_record,
+        fcst_id,
+        moe_code,
+        sales_org,
+        bus_sgmnt_code,
+        dmnd_grp_code,
+        zrep_matl_code,
+        mars_week,
+        start_date,
+        end_date,
+        dmnd_plng_node,
+        px_dmnd_plng_node,
+        split_qty,
+        created_date,
+        modified_date,
+        has_px_account_sku,
+        has_px_account,
+        has_px_sku
+      from table(dfnpxi01_extract_v2.pt_output(i_fcst_id))
+      order by
+        px_dmnd_plng_node,
+        zrep_matl_code,
+        start_date
+    )
+    loop
+      -- Create interface when required
+      if lics_outbound_loader.is_created = false then
+        lics_logging.write_log('INFO : Create Outbound Interface ['||v_interface_name_with_suffix||'] for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+        v_instance := lics_outbound_loader.create_interface(v_interface_name_with_suffix);
+      end if;
+
+    end loop;
+    -- Finalise interface when required
+    if lics_outbound_loader.is_created = true then
+      lics_logging.write_log('INFO : Finalise Outbound Interface ['||v_interface_name_with_suffix||'] for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+      lics_outbound_loader.finalise_interface;
+    end if;
+
+    -- Commit changes to product history extract table
+    lics_logging.write_log('INFO : COMMIT 355DMND History [px_355dmnd_history] for Forecast ['||i_fcst_id||'] MOE ['||v_moe_code||'].');
+    commit;
+
+    -- End Logging
+    lics_logging.end_log;
+
+  exception
+     when others then
+       rollback;
+       if lics_outbound_loader.is_created = true then
+         lics_outbound_loader.add_exception(substr(sqlerrm, 1, 512));
+         lics_outbound_loader.finalise_interface;
+       end if;
+       lics_logging.end_log;
+       raise;
+   end execute;
+
+end dfnapo01_extract; 
---
+++ SOURCE/IPS/BASE/ORACLE/DF_APP/DFNAPO01_EXTRACT.sql
@@ -0,0 +1,74 @@
+create or replace package dfnapo01_extract as
+/******************************************************************************/
+/* Package Definition                                                         */
+/******************************************************************************/
+/**
+  System  : IPS
+  Package : DFNPXI01_EXTRACT
+  Owner   : DF_APP
+  Author  : Chris Horn
+
+  Description
+  -----------
+  Demand Financials (Outbound) -> Apollo Supply (Inbound)
+
+  This interface creates and outbound domestic forecast extract to be 
+  sent to Apollo Supply. 
+
+  As the extract cannot handle negatives or decimals all quantites
+  are brought to zero and then rounded.
+
+  Date        Author                Description
+  ----------  --------------------  ---------------------------------------------
+  2014-08-22  Chris Horn            Created.
+
+*******************************************************************************/
+
+/*******************************************************************************
+  NAME: FORECAST_MOE                                                      PUBLIC
+  PURPOSE : Function to return [moe_code] for a given [fcst_id]
+*******************************************************************************/
+  function forecast_moe(
+    i_fcst_id in common.st_id
+  ) return common.st_code;
+
+/*******************************************************************************
+  NAME:  FIRST_DATE_FCST_CASTING_WEEK                                     PUBLIC
+*******************************************************************************/
+  function first_date_mars_week(
+    i_mars_week in common.st_count
+  ) return date;
+
+/*******************************************************************************
+  NAME: GET_FORECAST                                                      PUBLIC
+  PURPOSE : Pipeline to return the calculated forcast for interfacing.
+*******************************************************************************/
+  type rt_forecast is record (
+    -- Information Fields.
+    fcst_id                         fcst.fcst_id%type,
+    moe_code                        fcst.moe_code%type,
+    dmnd_grp_code                   dmnd_grp.dmnd_grp_code%type,
+    mars_week                       dmnd_data.mars_week%type,
+    -- Actual Output Fields.
+    tdu_matl_code                   dmnd_data.tdu%type,  -- Material Determined TDU code that needs to be shipped.
+    plant_code                      common.st_code, -- The plant code that we expect to ship this product from.
+    start_date                      date, -- This is the starting date in DD/MM/YYYY Sunday of this week. 
+    qty                             common.st_value -- This is the quantity that we expect to ship.  
+  );
+
+  type tt_forecast is table of rt_forecast;
+
+  function get_forecast (
+    i_fcst_id in common.st_id
+  ) return tt_forecast pipelined;
+
+
+/*******************************************************************************
+  NAME: EXECUTE                                                           PUBLIC
+  PURPOSE: Creates the outbound interface using get forecast.
+*******************************************************************************/
+  procedure execute(
+    i_fcst_id in common.st_id
+  );
+
+end dfnapo01_extract;
