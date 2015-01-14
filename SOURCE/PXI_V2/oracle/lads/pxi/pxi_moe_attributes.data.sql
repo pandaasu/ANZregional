628e1c7fb12b75d02bbf01806998255b93f9b4b8
hornchr <hornchr>
2015-01-14 02:05:40 +0000

    Updated with the correct moe attributes for the promax divisions.

--- SOURCE/PXI_V2/oracle/lads/pxi/pxi_moe_attributes.data.sql
+++ SOURCE/PXI_V2/oracle/lads/pxi/pxi_moe_attributes.data.sql
@@ -10,11 +10,11 @@ delete from pxi_moe_attributes;
 
 -- Populate Table
 insert into pxi_moe_attributes (moe_code,interface_suffix,px_company_code,px_division_code,location_code,system_code) 
-values ('009','1','147','55','AU','PX_AU_SNACK');
+values ('009','1','147','01','AU','PX_AU_SNACK');
 insert into pxi_moe_attributes (moe_code,interface_suffix,px_company_code,px_division_code,location_code,system_code) 
-values ('0021','2','147','57','AU','PX_AU_FOOD');
+values ('0021','2','147','02','AU','PX_AU_FOOD');
 insert into pxi_moe_attributes (moe_code,interface_suffix,px_company_code,px_division_code,location_code,system_code) 
-values ('0196','3','147','56','AU','PX_AU_PETCARE');
+values ('0196','3','147','05','AU','PX_AU_PETCARE');
 insert into pxi_moe_attributes (moe_code,interface_suffix,px_company_code,px_division_code,location_code,system_code) 
 values ('0086','4','149','149','NZ','PX_NZ');
 
