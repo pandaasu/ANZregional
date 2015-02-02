8e9f488a825ccfc26ce19d562d4c838f945051c4
keenaben <keenaben>
2015-02-02 03:59:00 +0000

    (CD - 2 Feb 2014) rpad put in place to deal with truncated rows being delivered from Apollo

--- SOURCE/PXI_V2/oracle/lads/pxi_app/apopxi01_loader.body.sql
+++ SOURCE/PXI_V2/oracle/lads/pxi_app/apopxi01_loader.body.sql
@@ -1,7 +1,7 @@
-create or replace package body apopxi01_loader as
+CREATE OR REPLACE package body PXI_APP.apopxi01_loader as
 /*******************************************************************************
   Package Constants
-*******************************************************************************/  
+*******************************************************************************/
   -- Package Name
   pc_package_name      constant pxi_common.st_package_name := 'APOPXI01_LOADER';
 
@@ -19,7 +19,7 @@ create or replace package body apopxi01_loader as
 
 /*******************************************************************************
   Package Variables
-*******************************************************************************/  
+*******************************************************************************/
   pv_interface_suffix    fflu_common.st_interface;
   pv_expected_moe_code   pxi_common.st_moe_code;
   pv_first_row           boolean;
@@ -33,12 +33,12 @@ create or replace package body apopxi01_loader as
     -- Get interface suffix
     pv_interface_suffix := fflu_app.fflu_utils.get_interface_suffix;
     -- Check that the interface suffix is valid.
-    if pxi_e2e_demand.is_valid_suffix(pv_interface_suffix) = false then 
+    if pxi_e2e_demand.is_valid_suffix(pv_interface_suffix) = false then
       fflu_data.log_interface_error('Interface Suffix',pv_interface_suffix,'Interface Suffix was not a valid value. Check PXI_MOE_ATTRIBUTES table for a list of valid interface suffixes');
     end if;
     -- Now look up the expected moe code.
     pv_expected_moe_code := pxi_e2e_demand.get_moe_from_suffix(pv_interface_suffix);
-    
+
     -- Now initialise the data parsing package.
     fflu_data.initialise(on_get_file_type,on_get_csv_qualifier,fflu_data.gc_no_file_header,fflu_data.gc_not_allow_missing);
 
@@ -46,17 +46,17 @@ create or replace package body apopxi01_loader as
     fflu_data.add_char_field_txt(pc_field_demand_unit,1,16,3,fflu_data.gc_not_allow_null,fflu_data.gc_trim);
     fflu_data.add_char_field_txt(pc_field_demand_group,17,7,fflu_data.gc_null_min_length,fflu_data.gc_not_allow_null,fflu_data.gc_trim);
     fflu_data.add_char_field_txt(pc_field_location_code,24,5,fflu_data.gc_null_min_length,fflu_data.gc_not_allow_null,fflu_data.gc_trim);
-    fflu_data.add_date_field_txt(pc_field_load_date,29,8,'yyyymmdd',fflu_data.gc_null_min_date,fflu_data.gc_null_max_date,fflu_data.gc_not_allow_null); 
+    fflu_data.add_date_field_txt(pc_field_load_date,29,8,'yyyymmdd',fflu_data.gc_null_min_date,fflu_data.gc_null_max_date,fflu_data.gc_not_allow_null);
     fflu_data.add_date_field_txt(pc_field_start_date,43,8,'yyyymmdd',fflu_data.gc_null_min_date,fflu_data.gc_null_max_date,fflu_data.gc_not_allow_null);
     fflu_data.add_number_field_txt(pc_field_duration,57,5,fflu_data.gc_null_format,fflu_data.gc_null_min_number,fflu_data.gc_null_max_number,fflu_data.gc_not_allow_null);
     fflu_data.add_number_field_txt(pc_field_type_code,62,1,fflu_data.gc_null_format,fflu_data.gc_null_min_number,fflu_data.gc_null_max_number,fflu_data.gc_not_allow_null);
     fflu_data.add_number_field_txt(pc_field_quantity,63,20,fflu_data.gc_null_format,fflu_data.gc_null_min_number,fflu_data.gc_null_max_number,fflu_data.gc_not_allow_null);
     fflu_data.add_char_field_txt(pc_field_forecast_text,83,50,fflu_data.gc_null_min_length,fflu_data.gc_allow_null,fflu_data.gc_trim);
     fflu_data.add_char_field_txt(pc_field_promotion_type,133,255,fflu_data.gc_null_min_length,fflu_data.gc_allow_null,fflu_data.gc_trim);
-    
+
     -- Flag the first row will be next.
     pv_first_row := true;
-    
+
   exception
     when others then
       fflu_data.log_interface_exception('ON_START');
@@ -69,9 +69,9 @@ create or replace package body apopxi01_loader as
     -- Row Variables
     rv_detail pxi_demand_detail%rowtype;
     v_ignore_row boolean;
-    
+
     -- This procedure perform the header creation.
-    procedure create_header is 
+    procedure create_header is
     begin
       -- Initialise the demand sequence
       select pxi_demand_seq.nextval into prv_header.demand_seq from dual;
@@ -86,28 +86,29 @@ create or replace package body apopxi01_loader as
       -- Now insert the header record into the database.
       insert into pxi_demand_header values prv_header;
     end create_header;
-    
+
   begin
     -- Now parse the incoming record.
-    if fflu_data.parse_data(p_row) = true then
+       -- (CD - 2 Feb 2014) rpad put in place to deal with truncated rows being delivered from Apollo
+    if fflu_data.parse_data(rpad(p_row,387,' ')) = true then
       -- Initialise Row Ignoring Variable.
       v_ignore_row := false;
-      
+
       -- Perform an expected moe check on the data.
-      if pv_expected_moe_code <> pxi_e2e_demand.get_moe_from_demand_unit(fflu_data.get_char_field(pc_field_demand_unit)) then 
+      if pv_expected_moe_code <> pxi_e2e_demand.get_moe_from_demand_unit(fflu_data.get_char_field(pc_field_demand_unit)) then
         fflu_data.log_field_error(pc_field_demand_unit,'MOE Code did not match the expected moe code of [' || pv_expected_moe_code || '] for this interface.');
       end if;
 
       -- If this is the first row that was successfully parsed.
-      if pv_first_row = true then 
+      if pv_first_row = true then
         -- Allocate a new demand sequence number.
-        if fflu_data.was_errors = false then 
+        if fflu_data.was_errors = false then
           create_header;
         end if;
         -- Now clear the first row processing flag.
         pv_first_row := false;
       end if;
-      
+
       -- Complete the data assignment into the array.
       rv_detail.demand_seq := prv_header.demand_seq;
       rv_detail.row_seq := fflu_utils.get_interface_row;
@@ -127,36 +128,36 @@ create or replace package body apopxi01_loader as
       if prv_header.location_code <> fflu_data.get_char_field(pc_field_location_code) then
         fflu_data.log_field_error(pc_field_location_code,'Location code did not match the location code of the first row of data [' || prv_header.location_code || '].');
       end if;
-      
+
       -- Check if the ZREP is valid.
-      if pxi_e2e_demand.is_valid_zrep(rv_detail.zrep_code) = false then 
+      if pxi_e2e_demand.is_valid_zrep(rv_detail.zrep_code) = false then
         -- fflu_data.log_field_error(pc_field_demand_unit,'Was not a valid ZREP Material Code.'); -- TODO ADD THIS LINE FOR PRODUCTION.
         v_ignore_row := true; -- TODO REMOVE FOR PRODUCTION.  This is to cater for dud test data.
       end if;
-      
+
       -- Now perform the ignore row check.
       if rv_detail.type_code not in (pxi_e2e_demand.gc_type_1_base,pxi_e2e_demand.gc_type_4_reconcile,pxi_e2e_demand.gc_type_6_override) or rv_detail.qty = 0 then
         v_ignore_row := true;
-      else 
+      else
         -- Now perform a minimum maximum mars week check.
-        if prv_header.min_mars_week is null then 
+        if prv_header.min_mars_week is null then
           prv_header.min_mars_week := rv_detail.mars_week;
-        else 
-          if rv_detail.mars_week < prv_header.min_mars_week then 
+        else
+          if rv_detail.mars_week < prv_header.min_mars_week then
             prv_header.min_mars_week := rv_detail.mars_week;
           end if;
         end if;
-        if prv_header.max_mars_week is null then 
+        if prv_header.max_mars_week is null then
           prv_header.max_mars_week := rv_detail.mars_week;
-        else 
-          if rv_detail.mars_week > prv_header.max_mars_week then 
+        else
+          if rv_detail.mars_week > prv_header.max_mars_week then
             prv_header.max_mars_week := rv_detail.mars_week;
           end if;
         end if;
       end if;
-      
+
       -- If a row was successfully parsed and there have been no errors so far.
-      if fflu_data.was_errors = false and v_ignore_row = false then 
+      if fflu_data.was_errors = false and v_ignore_row = false then
         insert into pxi_demand_detail values rv_detail;
       end if;
     end if;
@@ -169,13 +170,13 @@ create or replace package body apopxi01_loader as
   NAME:      ON_END                                                       PUBLIC
 *******************************************************************************/
   procedure on_end is
-  
+
     procedure update_header is
     begin
       -- Now set the minimum and maximum mars weeks that were detected.
-      if prv_header.demand_seq is not null then 
-        update pxi_demand_header 
-        set 
+      if prv_header.demand_seq is not null then
+        update pxi_demand_header
+        set
           min_mars_week = prv_header.min_mars_week,
           max_mars_week = prv_header.max_mars_week,
           modify_date = sysdate
@@ -183,11 +184,11 @@ create or replace package body apopxi01_loader as
           demand_seq = prv_header.demand_seq;
       end if;
     end update_header;
-  
+
   begin
     -- Update Header.
     update_header;
-    
+
     -- Only perform a commit if there were no errors at all.
     if fflu_data.was_errors then
       rollback;
