eab0649cfaa033bdcf453af5bcba967c6a09acd1
wuder <wuder>
2015-11-23 02:50:31 +0000

    re-fresh

--- SOURCE/LICS/BASE/ORACLE/LICS/v32/Table/JAVA_UTILITY.sql
+++ SOURCE/LICS/BASE/ORACLE/LICS/v32/Table/JAVA_UTILITY.sql
@@ -4,16 +4,12 @@
 
   CREATE OR REPLACE PACKAGE "LICS"."JAVA_UTILITY" as
 
-   /*-*
-   * Public declarations
-   *-*/
+   /*-*/
+   /* Public declarations
+   /*-*/
    procedure execute_external_procedure(par_command in varchar2);
    function execute_external_function(par_command in varchar2) return varchar2;
 
 end java_utility;
-
---------------------------------------------------------
---  DDL for Package Body JAVA_UTILITY
---------------------------------------------------------
 
 /
--- SOURCE/LICS/BASE/ORACLE/LICS/v32/Table/JAVA_UTILITY_1.sql
+++ SOURCE/LICS/BASE/ORACLE/LICS/v32/Table/JAVA_UTILITY_1.sql
@@ -4,15 +4,15 @@
 
   CREATE OR REPLACE PACKAGE BODY "LICS"."JAVA_UTILITY" as
 
-  /************************************************************
-   * This procedure performs the external procedure execution *
-   ************************************************************/
+   /************************************************************/
+   /* This procedure performs the external procedure execution */
+   /************************************************************/
    procedure execute_external_procedure(par_command in varchar2)
       as language java name 'com.isi.ics.cExternalProcess.executeProcedure(java.lang.String)';
 
-   /**********************************************************
-   * This function performs the external function execution *
-   **********************************************************/
+   /**********************************************************/
+   /* This function performs the external function execution */
+   /**********************************************************/
    function execute_external_function(par_command in varchar2) return varchar2
       as language java name 'com.isi.ics.cExternalProcess.executeFunction(java.lang.String) return java.lang.String';
 
