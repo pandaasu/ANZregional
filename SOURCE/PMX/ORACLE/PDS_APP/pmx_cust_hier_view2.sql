7c25fddfc36ba9a4362bbf3556709cebfdebca53
gordoste <gordoste>
2016-10-11 02:49:37 +0000

    Cleanup

--- SOURCE/PMX/ORACLE/PDS_APP/pmx_cust_hier_view2.sql
+++
@@ -1,4 +0,0 @@
--- Code to re-create the pmx_cust_hier_view containing the new columns added in LADS
-  CREATE OR REPLACE FORCE VIEW "PDS_APP"."PMX_CUST_HIER_VIEW" ("HDRSEQ", "COCODE", "REGION", "CHNCODE", "DIVCODE", "CUSTLEVEL", "CUSTNO", "POSFORMAT", "CHAIN", "EFF_FROM", "HDRDAT") AS 
-  select "HDRSEQ","COCODE","REGION","CHNCODE","DIVCODE","CUSTLEVEL","CUSTNO","POSFORMAT","CHAIN","EFF_FROM","HDRDAT" from lads.promax_cust_hier_view@ap0064p.world;
- 
