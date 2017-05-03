92f9f1ca916880c36c9e09317f1386f7841767d7
halicben <halicben>
2017-05-03 00:29:30 +0000

    Added additional methods to test webservice data retrieval.

--- SOURCE/FIDB/Webservice/PlantWebService.TestSite/Controllers/HomeController.cs
+++ SOURCE/FIDB/Webservice/PlantWebService.TestSite/Controllers/HomeController.cs
@@ -25,6 +25,231 @@ namespace PlantWebService.TestSite.Controllers
             return View(model);
         }
 
+        public ActionResult RetrieveProcessOrder()
+        {
+            var viewModel = new RetrieveProcessOrderViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveMaterials(int DisplayTypes, int Modes, string SystemKey, string PlantCode)
+        {
+            var binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+
+            var endpoint = new EndpointAddress(Properties.Settings.Default.ServiceURL);
+            var svc = new FactoryService.FactoryServiceClient(binding, endpoint);
+
+            var result = svc.RetrieveMaterials((FactoryService.RetrieveMode)Modes, PlantCode, SystemKey);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(result.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, result);
+
+                if (DisplayTypes == 1)
+                {
+                    var model = new ResultViewModel();
+                    model.ResultXml = textWriter.ToString();
+                    return View("Result", model);
+                }
+                else
+                {
+                    this.Response.AddHeader("Content-Disposition", @"attachment; filename=result.xml");
+
+                    return new ContentResult()
+                    {
+                        ContentEncoding = Encoding.UTF8,
+                        ContentType = "application/text",
+                        Content = textWriter.ToString()
+                    };
+                }
+            }
+        }
+
+        public ActionResult RetrieveMaterials()
+        {
+            var viewModel = new RetrieveMaterialsViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveFactoryTransfers(int DisplayTypes, int Modes, string SystemKey, string BatchCode, string MatlCode)
+        {
+            var binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+
+            var endpoint = new EndpointAddress(Properties.Settings.Default.ServiceURL);
+            var svc = new FactoryService.FactoryServiceClient(binding, endpoint);
+
+            var result = svc.RetrieveFactoryTransfers(BatchCode, MatlCode, (FactoryService.RetrieveMode)Modes, SystemKey);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(result.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, result);
+
+                if (DisplayTypes == 1)
+                {
+                    var model = new ResultViewModel();
+                    model.ResultXml = textWriter.ToString();
+                    return View("Result", model);
+                }
+                else
+                {
+                    this.Response.AddHeader("Content-Disposition", @"attachment; filename=result.xml");
+
+                    return new ContentResult()
+                    {
+                        ContentEncoding = Encoding.UTF8,
+                        ContentType = "application/text",
+                        Content = textWriter.ToString()
+                    };
+                }
+            }
+        }
+
+        public ActionResult RetrieveFactoryTransfers()
+        {
+            var viewModel = new RetrieveFactoryTransfersViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveProcessOrder(int DisplayTypes, string SystemKey, string ProcessOrder)
+        {
+            var binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+
+            var endpoint = new EndpointAddress(Properties.Settings.Default.ServiceURL);
+            var svc = new FactoryService.FactoryServiceClient(binding, endpoint);
+
+            var result = svc.RetrieveProcessOrder(ProcessOrder, SystemKey);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(result.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, result);
+
+                if (DisplayTypes == 1)
+                {
+                    var model = new ResultViewModel();
+                    model.ResultXml = textWriter.ToString();
+                    return View("Result", model);
+                }
+                else
+                {
+                    this.Response.AddHeader("Content-Disposition", @"attachment; filename=result.xml");
+
+                    return new ContentResult()
+                    {
+                        ContentEncoding = Encoding.UTF8,
+                        ContentType = "application/text",
+                        Content = textWriter.ToString()
+                    };
+                }
+            }
+        }
+
+        public ActionResult RetrieveMaterialBatchList()
+        {
+            var viewModel = new RetrieveMaterialBatchListViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveMaterialBatchList(int DisplayTypes, int Modes, string SystemKey, string MatlCode, string BatchCode)
+        {
+            var binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+
+            var endpoint = new EndpointAddress(Properties.Settings.Default.ServiceURL);
+            var svc = new FactoryService.FactoryServiceClient(binding, endpoint);
+
+            var result = svc.RetrieveMaterialBatchList(BatchCode, MatlCode, (FactoryService.RetrieveMode)Modes, SystemKey);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(result.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, result);
+
+                if (DisplayTypes == 1)
+                {
+                    var model = new ResultViewModel();
+                    model.ResultXml = textWriter.ToString();
+                    return View("Result", model);
+                }
+                else
+                {
+                    this.Response.AddHeader("Content-Disposition", @"attachment; filename=result.xml");
+
+                    return new ContentResult()
+                    {
+                        ContentEncoding = Encoding.UTF8,
+                        ContentType = "application/text",
+                        Content = textWriter.ToString()
+                    };
+                }
+            }
+        }
+
+        public ActionResult RetrieveMarsCalendar()
+        {
+            var viewModel = new RetrieveMarsCalendarViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult RetrieveMarsCalendar(int DisplayTypes, int Modes, string SystemKey, DateTime? Date, int HistoryYears)
+        {
+            var binding = new BasicHttpBinding();
+            binding.MaxReceivedMessageSize = 2147483647;
+            binding.MaxBufferSize = 2147483647;
+
+            var endpoint = new EndpointAddress(Properties.Settings.Default.ServiceURL);
+            var svc = new FactoryService.FactoryServiceClient(binding, endpoint);
+
+            var result = svc.RetrieveMarsCalendar(Date, HistoryYears, (FactoryService.RetrieveMode)Modes, SystemKey);
+
+            XmlSerializer xmlSerializer = new XmlSerializer(result.GetType());
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, result);
+
+                if (DisplayTypes == 1)
+                {
+                    var model = new ResultViewModel();
+                    model.ResultXml = textWriter.ToString();
+                    return View("Result", model);
+                }
+                else
+                {
+                    this.Response.AddHeader("Content-Disposition", @"attachment; filename=result.xml");
+
+                    return new ContentResult()
+                    {
+                        ContentEncoding = Encoding.UTF8,
+                        ContentType = "application/text",
+                        Content = textWriter.ToString()
+                    };
+                }
+            }
+        }
+
         public ActionResult RetrieveProcessOrderList()
         {
             var viewModel = new RetrieveProcessOrderListViewModel();
@@ -70,7 +295,5 @@ namespace PlantWebService.TestSite.Controllers
                 }
             }
         }
-
-
     }
 }
