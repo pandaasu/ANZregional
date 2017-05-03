3b629ae85d4fc9467152561a229d107f8417ccba
halicben <halicben>
2017-05-03 00:30:15 +0000

    Added additional methods to test webservice data retrieval.

---
+++ SOURCE/FIDB/Webservice/PlantWebService.TestSite/ViewModels/RetrieveFactoryTransfersViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlantWebService.TestSite.ViewModels
+{
+    public class RetrieveFactoryTransfersViewModel
+    {
+        public RetrieveFactoryTransfersViewModel()
+        {
+            this.Modes = new List<SelectListItem>();
+            this.Modes.Add(new SelectListItem() { Text = "Delta", Value = "1" });
+            this.Modes.Add(new SelectListItem() { Text = "Full", Value = "2" });
+            this.Modes.Add(new SelectListItem() { Text = "Reset", Value = "3" });
+
+            this.DisplayTypes = new List<SelectListItem>();
+            this.DisplayTypes.Add(new SelectListItem() { Text = "Screen", Value = "1" });
+            this.DisplayTypes.Add(new SelectListItem() { Text = "Download", Value = "2" }); 
+        }
+
+        public List<SelectListItem> Modes { get; set; }
+        public List<SelectListItem> DisplayTypes { get; set; }
+
+        public int DisplayType { get; set; }
+        public int Mode { get; set; }
+
+        public string SystemKey { get; set; }
+        public string MatlCode { get; set; }
+        public string BatchCode { get; set; }
+    }
+}
---
+++ SOURCE/FIDB/Webservice/PlantWebService.TestSite/ViewModels/RetrieveMarsCalendarViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PlantWebService.TestSite.ViewModels
+{
+    public class RetrieveMarsCalendarViewModel
+    {
+        public RetrieveMarsCalendarViewModel()
+        {
+            this.Modes = new List<SelectListItem>();
+            this.Modes.Add(new SelectListItem() { Text = "Delta", Value = "1" });
+            this.Modes.Add(new SelectListItem() { Text = "Full", Value = "2" });
+            this.Modes.Add(new SelectListItem() { Text = "Reset", Value = "3" });
+
+            this.DisplayTypes = new List<SelectListItem>();
+            this.DisplayTypes.Add(new SelectListItem() { Text = "Screen", Value = "1" });
+            this.DisplayTypes.Add(new SelectListItem() { Text = "Download", Value = "2" });
+        }
+
+        public List<SelectListItem> Modes { get; set; }
+        public List<SelectListItem> DisplayTypes { get; set; }
+
+        public DateTime? Date { get; set; }
+        public int HistoryYears { get; set; }
+        public int Mode { get; set; }
+        public string SystemKey { get; set; }
+    }
+}
